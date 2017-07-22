27b3a11983338012d40f48ea3a502f6a7a6ab01e
Cardman <f.desrochettes@gmail.com>
2017-07-22 09:43:27 +0200

    clean dead code

--- converterimages/src/main/java/converterimages/main/MainClass.java
+++ converterimages/src/main/java/converterimages/main/MainClass.java
@@ -7,16 +7,6 @@ public final class MainClass {
 
     /**@param args*/
     public static void main(String[] args) {
-//        System.out.println(getForeignPath());
-////        Constants.setMainClass(Constants.class);
-//        StringList l_ = StreamZipFile.getFilesInJar();
-//        System.out.println(l_);
-////        Constants.setMainClass(SoftApplication.class);
-//        System.out.println(ConstFiles.getForeignPath());
-//        StringList l2_ = StreamZipFile.getFilesInJar();
-//        System.out.println(l2_);
-//        System.out.println(List.equalsSet(l2_, l_));
-//        System.out.println(java.lang.Runtime.getRuntime().maxMemory()/1024/1024/1024);
         launch();
     }
 
