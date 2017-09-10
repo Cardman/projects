51c86be61a6ee800349fd95673ba3f5f9cf07331
Cardman <f.desrochettes@gmail.com>
2017-09-10 19:47:56 +0200

    remove some static imports

--- datacheck/src/test/java/code/datacheck/ObjectComponentsTest.java
+++ datacheck/src/test/java/code/datacheck/ObjectComponentsTest.java
@@ -1,5 +1,4 @@
 package code.datacheck;
-import static code.util.opers.EquallableUtil.assertEq;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
@@ -67,6 +66,7 @@ public class ObjectComponentsTest {
     private static final String LS_CLASS = "a";
     private static final String MP_CLASS = "m";
     private static final String REF_ARRAY = RefsArray.class.getName();
+    private static final String DIFF = " != ";
 
     @Test
     public void isInterface1Test() {
@@ -1674,5 +1674,78 @@ public class ObjectComponentsTest {
         ObjectComponents.setCheckingNullity(true);
         ObjectComponents.setReferences(false);
         ObjectComponents.setupNotNull(list_);
+    }
+
+    public static void assertEq(String _expected, Object _result) {
+        if (checkNullity(_expected, _result)) {
+            return;
+        }
+        if (StringList.quickEq(_expected, (String)_result)) {
+            return;
+        }
+        assertError(_expected, _result);
+    }
+
+    private static boolean allNull(Object _expected, Object _result) {
+        return _expected == null && _result == null;
+    }
+
+    private static boolean onlyOneNull(Object _expected, Object _result) {
+        if (_expected == null) {
+            return _result != null;
+        }
+        return _result == null;
+    }
+    private static boolean sameValue(Object _expected, Object _result) {
+        boolean second_ = false;
+        if (_result instanceof Number) {
+            second_ = true;
+        }
+        if (_result instanceof Character) {
+            second_ = true;
+        }
+        if (!second_) {
+            return false;
+        }
+        if (_expected instanceof Double || _expected instanceof Float) {
+            if (_result instanceof Number) {
+                return ((Number)_expected).doubleValue() == ((Number)_result).doubleValue();
+            }
+            return false;
+        }
+        if (_expected instanceof Number && _result instanceof Number) {
+            return ((Number)_expected).longValue() == ((Number)_result).longValue();
+        }
+        if (_expected instanceof Number && _result instanceof Character) {
+            return ((Number)_expected).longValue() == ((Character)_result).charValue();
+        }
+        if (_expected instanceof Character && _result instanceof Number) {
+            return ((Character)_expected).charValue() == ((Number)_result).longValue();
+        }
+        return ((Character)_expected).charValue() == ((Character)_result).charValue();
+    }
+
+    public static void assertEq(Number _expected, Number _result) {
+        if (checkNullity(_expected, _result)) {
+            return;
+        }
+        if (sameValue(_expected, _result)) {
+            return;
+        }
+        assertError(_expected, _result);
+    }
+
+    private static boolean checkNullity(Object _expected, Object _result) {
+        if (allNull(_expected, _result)) {
+            return true;
+        }
+        if (onlyOneNull(_expected, _result)) {
+            throw new AssertionError(null);
+        }
+        return false;
+    }
+
+    private static void assertError(Object _expected, Object _result) {
+        throw new AssertionError(_expected+DIFF+_result);
     }
 }
