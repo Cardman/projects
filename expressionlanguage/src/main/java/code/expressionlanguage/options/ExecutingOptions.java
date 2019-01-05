601019c5763f345dfa27d391a03d02bc57cc9a7e
unknown <f.desrochettes@gmail.com>
2019-01-05 11:42:43 +0100

    remove tabulations

--- expressionlanguage/src/main/java/code/expressionlanguage/options/ExecutingOptions.java
+++ expressionlanguage/src/main/java/code/expressionlanguage/options/ExecutingOptions.java
@@ -8,29 +8,29 @@ public final class ExecutingOptions {
     private String mainThread = "main_thread.txt";
     private int tabWidth = 4;
     private AtomicBoolean interrupt = new AtomicBoolean();
-	public String getLogFolder() {
-		return logFolder;
-	}
-	public void setLogFolder(String _logFolder) {
-		logFolder = _logFolder;
-	}
-	public String getMainThread() {
-		return mainThread;
-	}
-	public void setMainThread(String _mainThread) {
-		mainThread = _mainThread;
-	}
-	public int getTabWidth() {
-		return tabWidth;
-	}
-	public void setTabWidth(int _tabWidth) {
-		tabWidth = _tabWidth;
-	}
-	public AtomicBoolean getInterrupt() {
-		return interrupt;
-	}
-	public void setInterrupt(AtomicBoolean _interrupt) {
-		interrupt = _interrupt;
-	}
+    public String getLogFolder() {
+        return logFolder;
+    }
+    public void setLogFolder(String _logFolder) {
+        logFolder = _logFolder;
+    }
+    public String getMainThread() {
+        return mainThread;
+    }
+    public void setMainThread(String _mainThread) {
+        mainThread = _mainThread;
+    }
+    public int getTabWidth() {
+        return tabWidth;
+    }
+    public void setTabWidth(int _tabWidth) {
+        tabWidth = _tabWidth;
+    }
+    public AtomicBoolean getInterrupt() {
+        return interrupt;
+    }
+    public void setInterrupt(AtomicBoolean _interrupt) {
+        interrupt = _interrupt;
+    }
 
 }
