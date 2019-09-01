5868f83534b299eda5b0bfc8817296bd1e22daec
Cardman <f.desrochettes@gmail.com>
2019-09-01 13:05:53 +0200

    add gui launcher

---
+++ expressionlanguageguimain/src/main/java/code/expressionlanguage/main/gui/MainClass.java
@@ -0,0 +1,9 @@
+package code.expressionlanguage.main.gui;
+
+import code.expressionlanguage.GuiProcess;
+
+public class MainClass {
+    public static void main(String[] _args) {
+        GuiProcess.launch(_args);
+    }
+}
