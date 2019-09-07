db418006a13d6ff097b11af7851db7a61f06e29b
Cardman <f.desrochettes@gmail.com>
2019-09-07 12:51:29 +0200

    improve thread safety by removing compare values string builders

--- expressionlanguage/src/main/java/code/expressionlanguage/structs/StringStruct.java
+++ expressionlanguage/src/main/java/code/expressionlanguage/structs/StringStruct.java
@@ -18,6 +18,9 @@ public final class StringStruct extends CharSequenceStruct {
     }
 
     public boolean sameReference(Struct _other) {
+        if (!(_other instanceof StringStruct)) {
+            return false;
+        }
         return sameEq(this, _other);
     }
     public static void instantiate(LgNames _stds, ResultErrorStd _res, ConstructorId _method, Struct... _args) {
