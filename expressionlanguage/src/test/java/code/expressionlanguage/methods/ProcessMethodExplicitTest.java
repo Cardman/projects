66fb43f82d6d5b01db19244082d07fe5b0a93aae
Cardman <f.desrochettes@gmail.com>
2020-06-01 09:42:01 +0200

    refactor code

--- expressionlanguage/src/test/java/code/expressionlanguage/methods/ProcessMethodExplicitTest.java
+++ expressionlanguage/src/test/java/code/expressionlanguage/methods/ProcessMethodExplicitTest.java
@@ -772,6 +772,39 @@ public final class ProcessMethodExplicitTest extends ProcessMethodCommon {
         assertEq(5, getNumber(ret_));
     }
     @Test
+    public void calculate___18Test() {
+        StringMap<String> files_ = new StringMap<String>();
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("public class pkg.Apply {\n");
+        xml_.append(" public static ExClass<int> method(){\n");
+        xml_.append("  return staticCall(ExCaller<int>).method();\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        xml_.append("public class pkg.ExCaller<S> {\n");
+        xml_.append(" public staticCall ExClass<S> method(){\n");
+        xml_.append("  return explicit(ExClass<S>,ExClass<T>,T)(S)5;\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        xml_.append("public class pkg.ExClass<T> {\n");
+        xml_.append(" public T field;\n");
+        xml_.append(" public static ExClass<T> explicit(T i){\n");
+        xml_.append("  ExClass<T> e = new ExClass<T>();\n");
+        xml_.append("  e.field = i;\n");
+        xml_.append("  return e;\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        files_.put("pkg/Ex", xml_.toString());
+        ContextEl cont_ = contextElReadOnlyDefault();
+        Classes.validateAll(files_, cont_);
+        assertTrue(cont_.isEmptyErrors());
+        CustList<Argument> args_ = new CustList<Argument>();
+        MethodId id_ = getMethodId("method");
+        Argument ret_ = calculateNormal("pkg.Apply", id_, args_, cont_);
+        Struct struct_ = ret_.getStruct();
+        assertEq("pkg.ExClass<int>", struct_.getClassName(cont_));
+        assertEq(5, ((IntStruct) getField(struct_, new ClassField("pkg.ExClass", "field"))).intStruct());
+    }
+    @Test
     public void calculate18Test() {
         StringMap<String> files_ = new StringMap<String>();
         StringBuilder xml_ = new StringBuilder();
