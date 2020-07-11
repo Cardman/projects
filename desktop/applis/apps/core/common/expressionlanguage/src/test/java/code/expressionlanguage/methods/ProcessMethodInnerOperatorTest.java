f99c0059bcb423cb7a4ad96fa6bb38e47802a891
Cardman <f.desrochettes@gmail.com>
2020-07-11 14:00:34 +0200

    prepare improve implicit casts for symbol operations

--- desktop/applis/apps/core/common/expressionlanguage/src/test/java/code/expressionlanguage/methods/ProcessMethodInnerOperatorTest.java
+++ desktop/applis/apps/core/common/expressionlanguage/src/test/java/code/expressionlanguage/methods/ProcessMethodInnerOperatorTest.java
@@ -779,6 +779,73 @@ public final class ProcessMethodInnerOperatorTest extends ProcessMethodCommon {
         ret_ = calculateNormal("pkg.Ex", id_, args_, cont_);
         assertEq(0, getNumber(ret_));
     }
+
+    @Test
+    public void calculateArgument25Test() {
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("$public $class pkg.ExSub:Ex {\n");
+        xml_.append("}\n");
+        xml_.append("$public $class pkg.Ex {\n");
+        xml_.append(" $public $int a;\n");
+        xml_.append(" $operator++ ExSub (pkg.Ex a){\n");
+        xml_.append("  ExSub b = $new ExSub();\n");
+        xml_.append("  b.a=a.a+1;\n");
+        xml_.append("  $return b;\n");
+        xml_.append(" }\n");
+        xml_.append(" $public $static $int catching(){\n");
+        xml_.append("  Ex one = $new ExSub();\n");
+        xml_.append("  one.a=5i;\n");
+        xml_.append("  $if ((one++).a == 5i){\n");
+        xml_.append("   $return one.a;\n");
+        xml_.append("  }\n");
+        xml_.append("  $return 0i;\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        StringMap<String> files_ = new StringMap<String>();
+        ContextEl cont_ = contextElDefault();
+        files_.put("pkg/Ex", xml_.toString());
+        Classes.validateAll(files_, cont_);
+        assertTrue(cont_.isEmptyErrors());
+        CustList<Argument> args_ = new CustList<Argument>();
+        MethodId id_ = getMethodId("catching");
+        Argument ret_;
+        ret_ = calculateNormal("pkg.Ex", id_, args_, cont_);
+        assertEq(6, getNumber(ret_));
+    }
+
+    @Test
+    public void calculateArgument26Test() {
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("$public $class pkg.ExSub:Ex {\n");
+        xml_.append("}\n");
+        xml_.append("$public $class pkg.Ex {\n");
+        xml_.append(" $public $int a;\n");
+        xml_.append(" $operator++ ExSub (pkg.Ex a){\n");
+        xml_.append("  ExSub b = $new ExSub();\n");
+        xml_.append("  b.a=a.a+1;\n");
+        xml_.append("  $return b;\n");
+        xml_.append(" }\n");
+        xml_.append(" $public $static $int catching(){\n");
+        xml_.append("  Ex one = $new ExSub();\n");
+        xml_.append("  one.a=5i;\n");
+        xml_.append("  $if ((++one).a == 6i){\n");
+        xml_.append("   $return one.a;\n");
+        xml_.append("  }\n");
+        xml_.append("  $return 0i;\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        StringMap<String> files_ = new StringMap<String>();
+        ContextEl cont_ = contextElDefault();
+        files_.put("pkg/Ex", xml_.toString());
+        Classes.validateAll(files_, cont_);
+        assertTrue(cont_.isEmptyErrors());
+        CustList<Argument> args_ = new CustList<Argument>();
+        MethodId id_ = getMethodId("catching");
+        Argument ret_;
+        ret_ = calculateNormal("pkg.Ex", id_, args_, cont_);
+        assertEq(6, getNumber(ret_));
+    }
+
     @Test
     public void calculateArgument1FailTest() {
         StringBuilder xml_ = new StringBuilder();
