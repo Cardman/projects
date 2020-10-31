b18e0bff188e5fa5305a93501c921a3c56aa5f21
Cardman <f.desrochettes@gmail.com>
2020-10-31 19:00:56 +0100

    refactor code

--- desktop/applis/apps/gui/guicommon/src/main/java/code/gui/initialize/ProgramInfos.java
+++ desktop/applis/apps/gui/guicommon/src/main/java/code/gui/initialize/ProgramInfos.java
@@ -41,10 +41,6 @@ public final class ProgramInfos implements AbstractProgramInfos {
     }
     private static String initialize(String _homePath) {
 
-        String userName_ = System.getProperty("user.name");
-
-        String tmpFolder_ = StringUtil.replaceBackSlashDot(System.getProperty(TMP_FOLDER_KEY));
-
         String init_ = StringUtil.replaceBackSlashDot(new File(DOT).getAbsolutePath());
 
         String javaPath_ = StringUtil.replaceBackSlash(System.getProperty("java.class.path"));
@@ -58,79 +54,77 @@ public final class ProgramInfos implements AbstractProgramInfos {
             //use tmpFolder as initFolder
             return init_.substring(0, init_.length() - 1);
         }
-        if (!_homePath.startsWith(SEPARATEUR)) {
-            String virtualStore_ = StringUtil.concat(_homePath,RELATIVE_VIRTUAL_STORE);
-            //<home>/AppData/Local/VirtualStore/
-            boolean isStandardProgramFilesExecuted_ = false;
-            if (init_.startsWith(C_PROGRAM_FILES)) {
-                isStandardProgramFilesExecuted_ = true;
-            } else if (init_.startsWith(C_PROGRAM_FILES_X86)) {
-                isStandardProgramFilesExecuted_ = true;
+        if (_homePath.startsWith(SEPARATEUR)) {
+            //unix os => jar folder path
+            String f_ = init_;
+            if (!f_.endsWith(SEPARATEUR)) {
+                f_ = StringUtil.concat(f_, SEPARATEUR);
             }
-            boolean storeVirtual_ = false;
-            if (new File(virtualStore_).exists()){
-                if (isStandardProgramFilesExecuted_) {
-                    storeVirtual_ = true;
-                }
-            }
-            if (storeVirtual_) {
-                //c:/program files/folder app
-                //c:/program files (x86)/folder app
-                StringList folders_ = new StringList();
-                for (String n: StringUtil.splitStrings(init_, SEPARATEUR)) {
-                    if (n.isEmpty()) {
-                        continue;
-                    }
-                    folders_.add(n);
-                }
-                StringList lastFolders_ = new StringList();
-                lastFolders_.add(folders_.get(1));
-                lastFolders_.add(folders_.last());
-                virtualStore_ = StringUtil.concat(virtualStore_, StringUtil.join(lastFolders_, SEPARATEUR));
-                new File(StringUtil.concat(virtualStore_, SEPARATEUR)).mkdirs();
-                return virtualStore_;
-            }
-            //is it c:/program files (x86)/folder/... app writable?
-            if (isStandardProgramFilesExecuted_) {
-                //jarFolder_.startsWith("C:/Program Files/")
-                //jarFolder_.startsWith("C:/Program Files (x86)/")
-                //jarFolder_ == C:/<pf>/<appfolder>
-                if (new File(init_).canWrite()) {
-                    return init_.substring(0, init_.length() - 1);
-                }
-            } else {
-                return init_.substring(0, init_.length() - 1);
-            }
-            //use temp folder
-            String tmpUserFolder_ = StringUtil.concat(tmpFolder_, userName_);
-            String realTmpUserFolder_;
-            if (!new File(tmpUserFolder_).exists()) {
-                realTmpUserFolder_ = tmpUserFolder_;
-            } else if (new File(tmpUserFolder_).isDirectory()) {
-                realTmpUserFolder_ = tmpUserFolder_;
-            } else {
-                int nb_ = 0;
-                while (true) {
-                    File f_ = new File(StringUtil.concatNbs(tmpUserFolder_,nb_));
-                    if (!f_.exists()) {
-                        break;
-                    }
-                    if (f_.isDirectory()) {
-                        break;
-                    }
-                    nb_++;
-                }
-                realTmpUserFolder_ = StringUtil.concatNbs(tmpUserFolder_,nb_);
-            }
-            new File(realTmpUserFolder_).mkdirs();
-            return realTmpUserFolder_;
+            return f_.substring(0, f_.length() - 1);
         }
-        //unix os => jar folder path
-        String f_ = init_;
-        if (!f_.endsWith(SEPARATEUR)) {
-            f_ = StringUtil.concat(f_, SEPARATEUR);
+        String virtualStore_ = StringUtil.concat(_homePath, RELATIVE_VIRTUAL_STORE);
+        //<home>/AppData/Local/VirtualStore/
+        boolean isStandardProgramFilesExecuted_ = false;
+        if (init_.startsWith(C_PROGRAM_FILES)) {
+            isStandardProgramFilesExecuted_ = true;
+        } else if (init_.startsWith(C_PROGRAM_FILES_X86)) {
+            isStandardProgramFilesExecuted_ = true;
         }
-        return f_.substring(0, f_.length() - 1);
+        //is it c:/program files (x86)/folder/... app writable?
+        if (!isStandardProgramFilesExecuted_) {
+            return init_.substring(0, init_.length() - 1);
+        }
+        if (new File(virtualStore_).exists()) {
+            //c:/program files/folder app
+            //c:/program files (x86)/folder app
+            StringList folders_ = new StringList();
+            for (String n : StringUtil.splitStrings(init_, SEPARATEUR)) {
+                if (n.isEmpty()) {
+                    continue;
+                }
+                folders_.add(n);
+            }
+            StringList lastFolders_ = new StringList();
+            lastFolders_.add(folders_.get(1));
+            lastFolders_.add(folders_.last());
+            virtualStore_ = StringUtil.concat(virtualStore_, StringUtil.join(lastFolders_, SEPARATEUR));
+            new File(StringUtil.concat(virtualStore_, SEPARATEUR)).mkdirs();
+            return virtualStore_;
+        }
+        //jarFolder_.startsWith("C:/Program Files/")
+        //jarFolder_.startsWith("C:/Program Files (x86)/")
+        //jarFolder_ == C:/<pf>/<appfolder>
+        if (new File(init_).canWrite()) {
+            return init_.substring(0, init_.length() - 1);
+        }
+
+        String userName_ = System.getProperty("user.name");
+
+        String tmpFolder_ = StringUtil.replaceBackSlashDot(System.getProperty(TMP_FOLDER_KEY));
+
+        //use temp folder
+        String tmpUserFolder_ = StringUtil.concat(tmpFolder_, userName_);
+        String realTmpUserFolder_;
+        if (!new File(tmpUserFolder_).exists()) {
+            realTmpUserFolder_ = tmpUserFolder_;
+        } else if (new File(tmpUserFolder_).isDirectory()) {
+            realTmpUserFolder_ = tmpUserFolder_;
+        } else {
+            int nb_ = 0;
+            while (true) {
+                File f_ = new File(StringUtil.concatNbs(tmpUserFolder_, nb_));
+                if (!f_.exists()) {
+                    break;
+                }
+                if (f_.isDirectory()) {
+                    break;
+                }
+                nb_++;
+            }
+            realTmpUserFolder_ = StringUtil.concatNbs(tmpUserFolder_, nb_);
+        }
+        new File(realTmpUserFolder_).mkdirs();
+        return realTmpUserFolder_;
     }
 
     public String getHomePath() {
