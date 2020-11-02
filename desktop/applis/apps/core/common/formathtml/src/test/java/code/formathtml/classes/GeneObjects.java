94ef7499467d7d0efdf2594897cccf0c64b8b0be
Cardman <f.desrochettes@gmail.com>
2020-11-02 18:50:13 +0100

    refactor code

--- desktop/applis/apps/core/common/formathtml/src/test/java/code/formathtml/classes/GeneObjects.java
+++ desktop/applis/apps/core/common/formathtml/src/test/java/code/formathtml/classes/GeneObjects.java
@@ -1,9 +1,18 @@
 package code.formathtml.classes;
 
 import code.util.CustList;
+import code.util.ints.Listable;
 
 public class GeneObjects extends CustList<Object> {
 
     public GeneObjects() {
+    }
+
+    public GeneObjects(Object... _c) {
+        super(_c);
+    }
+
+    public GeneObjects(Listable<Object> _c) {
+        super(_c);
     }
 }
