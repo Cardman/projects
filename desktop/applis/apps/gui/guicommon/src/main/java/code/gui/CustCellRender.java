a9475a8ce5b9822e1ecff6d7423150a9d088ce7b
Cardman <f.desrochettes@gmail.com>
2021-02-04 09:50:15 +0100

    refactor code

--- desktop/applis/apps/gui/guicommon/src/main/java/code/gui/CustCellRender.java
+++ desktop/applis/apps/gui/guicommon/src/main/java/code/gui/CustCellRender.java
@@ -7,8 +7,7 @@ public abstract class CustCellRender<T> {
             int _index, boolean _isSelected, boolean _cellHasFocus);
     public abstract int getHeight();
     public abstract int getWidth();
-    public void paintComponent(CustGraphics _g) {
-    }
+    public abstract void paintComponent(CustGraphics _g);
     public void paintComponent(PreparedLabel _component) {
         BufferedImage buff_ = new BufferedImage(getWidth(),getHeight(),BufferedImage.TYPE_INT_RGB);
         CustGraphics gr_ = new CustGraphics(buff_.getGraphics());
