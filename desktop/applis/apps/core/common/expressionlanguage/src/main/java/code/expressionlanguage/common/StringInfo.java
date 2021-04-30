84bd31989a6e5a17257485a6210705166dd94f6c
Cardman <f.desrochettes@gmail.com>
2021-04-30 10:36:44 +0200

    refactor code

--- desktop/applis/apps/core/common/expressionlanguage/src/main/java/code/expressionlanguage/common/StringInfo.java
+++ desktop/applis/apps/core/common/expressionlanguage/src/main/java/code/expressionlanguage/common/StringInfo.java
@@ -1,36 +1,5 @@
 package code.expressionlanguage.common;
 
-import code.util.CharList;
+public final class StringInfo extends CharSeqBlockInfo {
 
-public final class StringInfo {
-
-    private final CharList chars = new CharList();
-    private String found;
-    private boolean ko;
-    private final char[] builtUnicode = new char[4];
-
-
-    public CharList getChars() {
-        return chars;
-    }
-
-    public String getFound() {
-        return found;
-    }
-
-    public void setFound(String _found) {
-        found = _found;
-    }
-
-    public char[] getBuiltUnicode() {
-        return builtUnicode;
-    }
-
-    public boolean isKo() {
-        return ko;
-    }
-
-    public void setKo() {
-        ko = true;
-    }
 }
--- desktop/applis/apps/core/common/expressionlanguage/src/main/java/code/expressionlanguage/common/TextBlockInfo.java
+++ desktop/applis/apps/core/common/expressionlanguage/src/main/java/code/expressionlanguage/common/TextBlockInfo.java
@@ -1,41 +1,9 @@
 package code.expressionlanguage.common;
 
-import code.util.CharList;
-
-public final class TextBlockInfo {
-
-    private final CharList chars = new CharList();
-    private String found;
-    private boolean ko;
+public final class TextBlockInfo extends CharSeqBlockInfo {
     private boolean line;
-    private final char[] builtUnicode = new char[4];
     private int lastSpace;
     private boolean printable;
-
-
-    public CharList getChars() {
-        return chars;
-    }
-
-    public String getFound() {
-        return found;
-    }
-
-    public void setFound(String _found) {
-        found = _found;
-    }
-
-    public char[] getBuiltUnicode() {
-        return builtUnicode;
-    }
-
-    public boolean isKo() {
-        return ko;
-    }
-
-    public void setKo() {
-        ko = true;
-    }
 
     public boolean isLine() {
         return line;
