c5738947c81e4842b820b7bc54fa046c0dbd7ead
Cardman <f.desrochettes@gmail.com>
2021-06-24 14:41:50 +0200

    refactor code

--- desktop/applis/apps/sys_apps/other_fact/src/main/java/code/sys/impl/AdvGraphicListGenerator.java
+++ desktop/applis/apps/sys_apps/other_fact/src/main/java/code/sys/impl/AdvGraphicListGenerator.java
@@ -2,11 +2,12 @@ package code.sys.impl;
 
 import code.gui.AbsGraphicList;
 import code.gui.CustGrList;
+import code.gui.images.AbstractImageFactory;
 import code.gui.initialize.AbstractGraphicListGenerator;
 
 public final class AdvGraphicListGenerator<T> implements AbstractGraphicListGenerator<T> {
     @Override
-    public AbsGraphicList<T> create(boolean _simple) {
+    public AbsGraphicList<T> create(AbstractImageFactory _fact, boolean _simple) {
         return new CustGrList<>(_simple);
     }
 
