3578bb13870d714debaa4879c1c9303a2f292e3b
Cardman <f.desrochettes@gmail.com>
2022-04-06 18:08:33 +0200

    refactor code

--- desktop/applis/apps/core/common/expressionlanguage/src/main/java/code/expressionlanguage/analyze/AbstractAnnotationAnalysis.java
+++
@@ -1,7 +0,0 @@
-package code.expressionlanguage.analyze;
-
-import code.expressionlanguage.analyze.instr.OperationsSequence;
-import code.expressionlanguage.analyze.opers.OperationNode;
-
-public interface AbstractAnnotationAnalysis {
-}
