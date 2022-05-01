80dac0fdb185fbc8f72ec390e3d5ef8f10672453
Cardman <f.desrochettes@gmail.com>
2022-05-01 21:42:13 +0200

    fix read file

--- desktop/applis/apps/apps_util/streamcore/src/main/java/code/stream/core/DefBinFact.java
+++ desktop/applis/apps/apps_util/streamcore/src/main/java/code/stream/core/DefBinFact.java
@@ -23,8 +23,9 @@ public final class DefBinFact implements AbstractBinFact {
                 break;
             }
         }
+        byte[] bytes_ = reader_.getBytes();
         reader_.close();
-        return reader_.getBytes();
+        return bytes_;
     }
 
     @Override
