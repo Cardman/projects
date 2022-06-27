737c844779201c6f4fad8dd1c0f5deddc40109b5
Cardman <f.desrochettes@gmail.com>
2022-06-27 14:12:57 +0200

    refactor code

--- desktop/applis/apps/apps_util/expressionlanguageutil/src/main/java/code/expressionlanguage/utilcompo/stds/FctThreadJoin.java
+++ desktop/applis/apps/apps_util/expressionlanguageutil/src/main/java/code/expressionlanguage/utilcompo/stds/FctThreadJoin.java
@@ -10,18 +10,19 @@ import code.expressionlanguage.structs.BooleanStruct;
 import code.expressionlanguage.structs.NullStruct;
 import code.expressionlanguage.structs.Struct;
 import code.expressionlanguage.utilcompo.ThreadStruct;
-import code.threads.AbstractThread;
 import code.threads.ThState;
 
 public final class FctThreadJoin implements StdCaller {
 
     @Override
     public ArgumentWrapper call(AbstractExiting _exit, ContextEl _cont, Struct _instance, ArgumentListCall _firstArgs, StackCall _stackCall) {
-        AbstractThread thread_ = ((ThreadStruct) _instance).getThread();
-        ThState state_ = thread_.join();
-        if (state_ == ThState.INTERRUPTED) {
+        return toResult(((ThreadStruct) _instance).getThread().join());
+    }
+
+    static ArgumentWrapper toResult(ThState _state) {
+        if (_state == ThState.INTERRUPTED) {
             return new ArgumentWrapper(NullStruct.NULL_VALUE);
         }
-        return new ArgumentWrapper(BooleanStruct.of(state_ == ThState.ALIVE));
+        return new ArgumentWrapper(BooleanStruct.of(_state == ThState.ALIVE));
     }
 }
