0458e286d5239495457ebba4bebe3f86fba5434a
Cardman <f.desrochettes@gmail.com>
2022-10-01 19:28:01 +0200

    add tests

--- desktop/applis/apps/bean/bean_games/presidentbean/src/test/java/cards/president/beans/PresidentBeanTest.java
+++ desktop/applis/apps/bean/bean_games/presidentbean/src/test/java/cards/president/beans/PresidentBeanTest.java
@@ -1,11 +1,18 @@
 package cards.president.beans;
 
 import cards.president.ResultsPresident;
+import code.bean.nat.NatDualConfigurationContext;
+import code.bean.nat.NativeConfigurationLoader;
+import code.bean.nat.fwd.DefNatBlockBuilder;
 import code.expressionlanguage.structs.Struct;
-import code.util.Bytes;
-import code.util.CustList;
-import code.util.Longs;
-import code.util.StringList;
+import code.formathtml.Configuration;
+import code.formathtml.Navigation;
+import code.formathtml.analyze.blocks.AnaRendBlock;
+import code.scripts.pages.cards.MessPresidentPage;
+import code.scripts.pages.cards.PagesPresidents;
+import code.sml.Document;
+import code.util.*;
+import code.util.consts.Constants;
 import org.junit.Test;
 
 public final class PresidentBeanTest extends BeanPresidentCommonTs {
@@ -34,6 +41,34 @@ public final class PresidentBeanTest extends BeanPresidentCommonTs {
         assertEq(1,res_,1,1);
         assertEq(3,res_,1,2);
         assertEq(4,res_,1,3);
+    }
+    @Test
+    public void init() {
+        StringMap<String> other_ = MessPresidentPage.ms();
+        AnaRendBlock.adjust(other_);
+        PresidentStandardsResults stds_ = new PresidentStandardsResults();
+        Navigation nav_ = new Navigation();
+        nav_.setSession(new Configuration());
+        nav_.setLanguage(EN);
+        nav_.setLanguages(Constants.getAvailableLanguages());
+        NativeConfigurationLoader nat_ = new NativeConfigurationLoader(stds_, new ResultsPresidentLoader());
+        Configuration session_ = new Configuration();
+        NatDualConfigurationContext d_ = nat_.getDualConfigurationContext(session_);
+        nat_.getForwards();
+        d_.init(session_);
+        nav_.setSession(session_);
+        StringMap<String> files_ = NatDualConfigurationContext.files(nav_,d_,other_,other_,"");
+        String realFilePath_ = session_.getFirstUrl();
+        StringMap<Document> docs_ = NatDualConfigurationContext.docs(PagesPresidents.build(),"");
+        session_.setFirstUrl(realFilePath_);
+        nav_.setFiles(files_);
+        stds_.setupAll(docs_,nav_, nav_.getSession(), new DefNatBlockBuilder(), d_);
+        stds_.setDataBase(build(fourPseudos("0", "1", "2", "3"), oneDeal(1, 3, 2, 4), (byte) 2, (byte) 1, (byte) 3, (byte) 4));
+        stds_.initializeRendSessionDoc(nav_);
+        assertEq("<html xmlns:c=\"javahtml\"><head><title>Results</title><link href=\"resources_cards/css/president.css\" rel=\"stylesheet\" type=\"text/css\"/><style>h1 {\n" +
+                "\tcolor: #0000FF;\n" +
+                "}\n" +
+                "</style></head><body><table border=\"1\"><caption>Ranks</caption><thead><tr><td/><td>0</td><td>1</td><td>2</td><td>3</td></tr></thead><tbody><tr><td>0</td><td>1</td><td>3</td><td>2</td><td>4</td></tr><tr><td>1</td><td>2</td><td>1</td><td>3</td><td>4</td></tr></tbody></table><br/></body></html>",nav_.getHtmlText());
     }
     private static CustList<Longs> oneDeal(long..._values) {
         CustList<Longs> ps_ = new CustList<Longs>();
--- desktop/applis/apps/bean/bean_games/presidentbean/src/test/java/cards/president/beans/RulesPresidentBeanTest.java
+++ desktop/applis/apps/bean/bean_games/presidentbean/src/test/java/cards/president/beans/RulesPresidentBeanTest.java
@@ -3,12 +3,18 @@ package cards.president.beans;
 import cards.consts.MixCardsChoice;
 import cards.president.RulesPresident;
 import cards.president.enumerations.EqualtyPlaying;
+import code.bean.nat.NatDualConfigurationContext;
+import code.bean.nat.NativeConfigurationLoader;
+import code.bean.nat.fwd.DefNatBlockBuilder;
 import code.expressionlanguage.structs.Struct;
+import code.formathtml.Configuration;
+import code.formathtml.Navigation;
 import code.formathtml.analyze.blocks.AnaRendBlock;
 import code.scripts.pages.cards.MessPresidentPage;
 import code.scripts.pages.cards.PagesPresidents;
 import code.sml.Document;
 import code.util.StringMap;
+import code.util.consts.Constants;
 import org.junit.Test;
 
 public final class RulesPresidentBeanTest extends BeanPresidentCommonTs {
@@ -131,13 +137,34 @@ public final class RulesPresidentBeanTest extends BeanPresidentCommonTs {
         assertEq(11,callRulesPresidentBeanNbCardsPerPlayerMax(bean_));
     }
 
-//    @Test
-//    public void init() {
-//        StringMap<String> other_ = MessPresidentPage.ms();
-//        AnaRendBlock.adjust(other_);
-//        StringMap<Document> docs_ = PagesPresidents.build();
-//        new PresidentStandardsRules();
-//    }
+    @Test
+    public void init() {
+        StringMap<String> other_ = MessPresidentPage.ms();
+        AnaRendBlock.adjust(other_);
+        PresidentStandardsRules stds_ = new PresidentStandardsRules();
+        Navigation nav_ = new Navigation();
+        nav_.setSession(new Configuration());
+        nav_.setLanguage(EN);
+        nav_.setLanguages(Constants.getAvailableLanguages());
+        NativeConfigurationLoader nat_ = new NativeConfigurationLoader(stds_, new RulesPresidentLoader());
+        Configuration session_ = new Configuration();
+        NatDualConfigurationContext d_ = nat_.getDualConfigurationContext(session_);
+        nat_.getForwards();
+        d_.init(session_);
+        nav_.setSession(session_);
+        StringMap<String> files_ = NatDualConfigurationContext.files(nav_,d_,other_,other_,"");
+        String realFilePath_ = session_.getFirstUrl();
+        StringMap<Document> docs_ = NatDualConfigurationContext.docs(PagesPresidents.buildRules(),"");
+        session_.setFirstUrl(realFilePath_);
+        nav_.setFiles(files_);
+        stds_.setupAll(docs_,nav_, nav_.getSession(), new DefNatBlockBuilder(), d_);
+        stds_.setDataBaseRules(rules(true, true, true, true, true, 4, 1));
+        stds_.initializeRendSessionDoc(nav_);
+        assertEq("<html xmlns:c=\"javahtml\"><head><link href=\"resources_cards/css/president.css\" rel=\"stylesheet\" type=\"text/css\"/><style>h1 {\n" +
+                "\tcolor: #0000FF;\n" +
+                "}\n" +
+                "</style></head><body><h1>Mix Cards</h1>at each launching<br/><h1>Number of players</h1>4<br/><h1>Number of stacks</h1>1<br/>Number of cards per player: 13<br/><br/><h1>Equality</h1>forbidden<br/><h1>Possible reversing strength</h1>yes<br/><h1>Every player has to play if possible</h1>yes<br/><h1>A player looses if this player finish by the best cards</h1>yes<br/><h1>For next deals, players can switch their cards at the beginning</h1>yes<br/><h1>For next deals, the looser player starts first the first trick</h1>yes<br/></body></html>",nav_.getHtmlText());
+    }
 
 
     private RulesPresident rules() {
