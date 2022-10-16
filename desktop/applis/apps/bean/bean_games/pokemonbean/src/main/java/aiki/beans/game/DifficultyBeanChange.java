974395d755f8fd124262cce9e004c04e11f21b98
Cardman <f.desrochettes@gmail.com>
2022-10-16 12:38:17 +0200

    refactor code

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/main/java/aiki/beans/game/DifficultyBeanChange.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/main/java/aiki/beans/game/DifficultyBeanChange.java
@@ -1,13 +1,14 @@
 package aiki.beans.game;
 
+import aiki.beans.CommonBean;
 import aiki.beans.PokemonBeanStruct;
 import code.bean.nat.NatCaller;
-import code.expressionlanguage.structs.NullStruct;
+import code.expressionlanguage.structs.StringStruct;
 import code.expressionlanguage.structs.Struct;
 public class DifficultyBeanChange implements NatCaller{
     @Override
     public Struct re(Struct _instance, Struct[] _args){
         ( (DifficultyBean) ((PokemonBeanStruct)_instance).getInstance()).change();
-        return NullStruct.NULL_VALUE;
+        return new StringStruct(CommonBean.DEST_WEB_GAME_HTML_DIFFICULTY_HTML);
     }
 }
--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/code/scripts/confs/BeanScPkTest.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/code/scripts/confs/BeanScPkTest.java
@@ -14,16 +14,13 @@ public final class BeanScPkTest extends EquallablePkBeanUtil {
         NatDualConfigurationContext d_ = new NatDualConfigurationContext();
         d_.setNavigation(PkScriptPagesInit.initConfData(configuration_));
         d_.setNavigation(PkScriptPagesInit.initConfFight(configuration_));
-        d_.setNavigation(PkScriptPagesInit.initConfDiff(configuration_));
         d_.setNavigation(PkScriptPagesInit.initConfProg(configuration_));
         assertNotNull(configuration_.getBeansInfos());
         PkScriptPages.initAnaData(d_);
         PkScriptPages.initAnaFight(d_);
-        PkScriptPages.initAnaDiff(d_);
         PkScriptPages.initAnaProg(d_);
         assertNotNull(d_.getMessagesFolder());
         assertNotNull(PagesInit.build());
-        assertNotNull(PagesInit.buildDiff());
         assertNotNull(PagesInit.buildFight());
         assertNotNull(PagesInit.buildProg());
         assertNotNull(MessagesInit.ms());
