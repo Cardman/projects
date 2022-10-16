3e4ae41294d487ee148a603a05884f3ab80837d4
Cardman <f.desrochettes@gmail.com>
2022-10-16 12:23:11 +0200

    add tests

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/game/DifficultyBeanTest.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/game/DifficultyBeanTest.java
@@ -1,17 +1,25 @@
 package aiki.beans.game;
 
-import aiki.beans.CommonBean;
-import aiki.beans.DifficultyCommon;
-import aiki.beans.InitDbBean;
-import aiki.beans.PokemonBeanStruct;
+import aiki.beans.*;
 import aiki.db.DataBase;
 import aiki.facade.FacadeGame;
 import aiki.game.Game;
 import aiki.game.params.Difficulty;
 import aiki.game.params.enums.DifficultyModelLaw;
 import aiki.game.params.enums.DifficultyWinPointsFight;
+import code.bean.nat.NatHtmlPage;
 import code.expressionlanguage.structs.Struct;
+import code.formathtml.Navigation;
+import code.formathtml.analyze.blocks.AnaRendBlock;
 import code.maths.Rate;
+import code.scripts.pages.aiki.CssInit;
+import code.scripts.pages.aiki.MessagesInit;
+import code.scripts.pages.aiki.PagesInit;
+import code.sml.DocumentBuilder;
+import code.sml.Element;
+import code.util.StringList;
+import code.util.StringMap;
+import code.util.consts.Constants;
 import org.junit.Test;
 
 public final class DifficultyBeanTest extends InitDbBean {
@@ -480,6 +488,108 @@ public final class DifficultyBeanTest extends InitDbBean {
         assertEq(CommonBean.DEST_WEB_GAME_HTML_DIFFICULTY_HTML,navigateDiffChange(callDifficultyBeanDiffWinningExpPtsFightSet(displaying(beanDiff(EN, fac_)),DifficultyWinPointsFight.FACILE.getWinName())));
         assertEq(DifficultyWinPointsFight.FACILE.getWinName(),diff(fac_).getDiffWinningExpPtsFight().getWinName());
     }
+    @Test
+    public void init() {
+        FacadeGame fac_ = facTr();
+        diff(fac_).setDiffWinningExpPtsFight(DifficultyWinPointsFight.FACILE);
+        diff(fac_).setDamageRatePlayer(DifficultyModelLaw.CONSTANT_MAX);
+        diff(fac_).setDamageRateLawFoe(DifficultyModelLaw.CONSTANT_MIN);
+        diff(fac_).setRateLooseMoneyWin(rt());
+        diff(fac_).setWinTrainerExp(rt());
+        diff(fac_).setRateWinningExpPtsFight(rt());
+        diff(fac_).setRateWinMoneyBase(rt());
+        StringMap<String> builtMessages_ = MessagesInit.ms();
+        AnaRendBlock.adjust(builtMessages_);
+        StringMap<String> builtOther_ = CssInit.ms();
+        PkDiff pk_ = new PkDiff();
+        Navigation nav_ = pk_.nav(Constants.getAvailableLanguages(),EN,new DiffGameInit(), PagesInit.buildDiff(),builtOther_,builtMessages_,ACCESS_TO_DEFAULT_FILES);
+        pk_.setDataBase(fac_);
+        pk_.initializeRendSessionDoc(nav_);
+        assertEq("<html xmlns:c=\"javahtml\"><head><title>Difficulty choice</title><link href=\"web_game/css/difficulty.css\" rel=\"stylesheet\" type=\"text/css\"/><style>p{\n" +
+                "\ttext-indent:25px;\n" +
+                "}\n" +
+                "body{\n" +
+                "\ttext-align:justify;\n" +
+                "}\n" +
+                "td{\n" +
+                "\tborder:1px solid black;\n" +
+                "}\n" +
+                "th{\n" +
+                "\tbackground: yellow;\n" +
+                "\tborder:1px solid black;\n" +
+                "}\n" +
+                "table{\n" +
+                "\tborder-spacing:0;\n" +
+                "}\n" +
+                "h1{\n" +
+                "\tcolor:red;\n" +
+                "}\n" +
+                "h2{\n" +
+                "\tcolor:blue;\n" +
+                "}\n" +
+                "span.errormessage{\n" +
+                "\tcolor:red;\n" +
+                "}\n" +
+                "</style></head><body><form c:command=\"difficulty.change\" name=\"changingDiff\" action=\"\" n-f=\"0\">Difficulty of winning points<select name=\"difficulty.diffWinningExpPtsFight\" n-i=\"0\"><option value=\"TRES_FACILE\">W1</option><option value=\"FACILE\" selected=\"selected\">W2</option><option value=\"DIFFICILE\">W3</option><option value=\"TRES_DIFFICILE\">W4</option></select><br/>Allow catching ko pokemon<input name=\"difficulty.allowCatchingKo\" type=\"checkbox\" n-i=\"1\" checked=\"checked\"/><br/>Allow swicthing places at the front of battle at the end of round<input name=\"difficulty.allowedSwitchPlacesEndRound\" type=\"checkbox\" n-i=\"2\"/><br/>Rate of winning experience points de gain de points while a fight against a foe<input c:validator=\"rate_validator\" id=\"winTrainerExpRate\" name=\"difficulty.winTrainerExp\" type=\"text\" n-i=\"3\" value=\"2\"/><span c:for=\"winTrainerExpRate\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,rate_issue\"/><br/>Rate of winning experience points<input c:validator=\"rate_validator\" id=\"winExpRateFight\" name=\"difficulty.rateWinningExpPtsFight\" type=\"text\" n-i=\"4\" value=\"2\"/><span c:for=\"winExpRateFight\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,rate_issue\"/><br/>End of fight if a team is ko.<input name=\"difficulty.endFightIfOneTeamKo\" type=\"checkbox\" n-i=\"5\" checked=\"checked\"/><br/>Iv of your pokemon<input c:validator=\"short_validator\" id=\"ivPlayerId\" name=\"difficulty.ivPlayer\" type=\"text\" n-i=\"6\" value=\"31\"/><span c:for=\"ivPlayerId\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,short_issue\"/><br/>Iv of the pokemon of your foes<input c:validator=\"short_validator\" id=\"ivFoeId\" name=\"difficulty.ivFoe\" type=\"text\" n-i=\"7\" value=\"0\"/><span c:for=\"ivFoeId\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,short_issue\"/><br/>Rate of won money between winning money while a victory and winning base<input c:validator=\"rate_validator\" id=\"rateWinMoneyBaseId\" name=\"difficulty.rateWinMoneyBase\" type=\"text\" n-i=\"8\" value=\"2\"/><span c:for=\"rateWinMoneyBaseId\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,rate_issue\"/><br/>Rate of lost money between loss while a defeat and winning money while a victory<input c:validator=\"rate_validator\" id=\"rateLooseMoneyWinId\" name=\"difficulty.rateLooseMoneyWin\" type=\"text\" n-i=\"9\" value=\"2\"/><span c:for=\"rateLooseMoneyWinId\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,rate_issue\"/><br/>Healed moves of your pokemon at the end of fight<input name=\"difficulty.restoredMovesEndFight\" type=\"checkbox\" n-i=\"10\"/><br/>The moves wth single target can achieve any foe<input name=\"difficulty.enabledClosing\" type=\"checkbox\" n-i=\"11\" checked=\"checked\"/><br/>Random appearing pokemon<input name=\"difficulty.randomWildFight\" type=\"checkbox\" n-i=\"12\"/><br/>Flee always possible<input name=\"difficulty.stillPossibleFlee\" type=\"checkbox\" n-i=\"13\" checked=\"checked\"/><br/>Do not learnt the already known moves<input name=\"difficulty.skipLearningMovesWhileNotGrowingLevel\" type=\"checkbox\" n-i=\"14\" checked=\"checked\"/><br/>Choice of averages of damage rate for your pokemon<select name=\"difficulty.damageRatePlayer\" n-i=\"15\"><option value=\"CONSTANT_MIN\">M1</option><option value=\"CROISSANT\">M2</option><option value=\"UNIFORME\">M3</option><option value=\"DECROISSANT\">M4</option><option value=\"CONSTANT_MAX\" selected=\"selected\">M5</option></select><br/><table><thead><tr><th>Rate</th><th>Probability</th></tr></thead><tbody><tr><td>1</td><td>1</td></tr></tbody></table>Choix of averages of damage rate for the pokemon of your foes<select name=\"difficulty.damageRateLawFoe\" n-i=\"16\"><option value=\"CONSTANT_MIN\" selected=\"selected\">M1</option><option value=\"CROISSANT\">M2</option><option value=\"UNIFORME\">M3</option><option value=\"DECROISSANT\">M4</option><option value=\"CONSTANT_MAX\">M5</option></select><br/><table><thead><tr><th>Rate</th><th>Probability</th></tr></thead><tbody><tr><td>17/20</td><td>1</td></tr></tbody></table><input value=\"OK\" type=\"submit\"/></form></body></html>",nav_.getHtmlText());
+    }
+    @Test
+    public void nav() {
+        FacadeGame fac_ = facTr(16);
+        StringMap<String> builtMessages_ = MessagesInit.ms();
+        AnaRendBlock.adjust(builtMessages_);
+        StringMap<String> builtOther_ = CssInit.ms();
+        PkDiff pk_ = new PkDiff();
+        Navigation nav_ = pk_.nav(Constants.getAvailableLanguages(),EN,new DiffGameInit(), PagesInit.buildDiff(),builtOther_,builtMessages_,ACCESS_TO_DEFAULT_FILES);
+        pk_.setDataBase(fac_);
+        pk_.initializeRendSessionDoc(nav_);
+        NatHtmlPage htmlPage_ = pk_.getNatPage();
+        choose(htmlPage_, 0, DifficultyWinPointsFight.FACILE.getWinName());
+        choose(htmlPage_, 3, rt().toNumberString());
+        choose(htmlPage_, 4, rt().toNumberString());
+        choose(htmlPage_, 6, "12");
+        choose(htmlPage_, 7, "4");
+        choose(htmlPage_, 8, rt().toNumberString());
+        choose(htmlPage_, 9, rt().toNumberString());
+        choose(htmlPage_, 15, DifficultyModelLaw.CONSTANT_MAX.getModelName());
+        choose(htmlPage_, 16, DifficultyModelLaw.CONSTANT_MIN.getModelName());
+        htmlPage_.setUrl(0);
+        Element elt_ = DocumentBuilder.getFirstElementByAttribute(nav_.getDocument(), nav_.getSession().getRendKeyWords().getAttrNf(), Long.toString(htmlPage_.getUrl()));
+
+        pk_.execute(true,elt_,nav_);
+
+
+        assertEq("<html xmlns:c=\"javahtml\"><head><title>Difficulty choice</title><link href=\"web_game/css/difficulty.css\" rel=\"stylesheet\" type=\"text/css\"/><style>p{\n" +
+                "\ttext-indent:25px;\n" +
+                "}\n" +
+                "body{\n" +
+                "\ttext-align:justify;\n" +
+                "}\n" +
+                "td{\n" +
+                "\tborder:1px solid black;\n" +
+                "}\n" +
+                "th{\n" +
+                "\tbackground: yellow;\n" +
+                "\tborder:1px solid black;\n" +
+                "}\n" +
+                "table{\n" +
+                "\tborder-spacing:0;\n" +
+                "}\n" +
+                "h1{\n" +
+                "\tcolor:red;\n" +
+                "}\n" +
+                "h2{\n" +
+                "\tcolor:blue;\n" +
+                "}\n" +
+                "span.errormessage{\n" +
+                "\tcolor:red;\n" +
+                "}\n" +
+                "</style></head><body><form c:command=\"difficulty.change\" name=\"changingDiff\" action=\"\" n-f=\"0\">Difficulty of winning points<select name=\"difficulty.diffWinningExpPtsFight\" n-i=\"0\"><option value=\"TRES_FACILE\">W1</option><option value=\"FACILE\" selected=\"selected\">W2</option><option value=\"DIFFICILE\">W3</option><option value=\"TRES_DIFFICILE\">W4</option></select><br/>Allow catching ko pokemon<input name=\"difficulty.allowCatchingKo\" type=\"checkbox\" n-i=\"1\" checked=\"checked\"/><br/>Allow swicthing places at the front of battle at the end of round<input name=\"difficulty.allowedSwitchPlacesEndRound\" type=\"checkbox\" n-i=\"2\"/><br/>Rate of winning experience points de gain de points while a fight against a foe<input c:validator=\"rate_validator\" id=\"winTrainerExpRate\" name=\"difficulty.winTrainerExp\" type=\"text\" n-i=\"3\" value=\"2\"/><span c:for=\"winTrainerExpRate\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,rate_issue\"/><br/>Rate of winning experience points<input c:validator=\"rate_validator\" id=\"winExpRateFight\" name=\"difficulty.rateWinningExpPtsFight\" type=\"text\" n-i=\"4\" value=\"2\"/><span c:for=\"winExpRateFight\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,rate_issue\"/><br/>End of fight if a team is ko.<input name=\"difficulty.endFightIfOneTeamKo\" type=\"checkbox\" n-i=\"5\" checked=\"checked\"/><br/>Iv of your pokemon<input c:validator=\"short_validator\" id=\"ivPlayerId\" name=\"difficulty.ivPlayer\" type=\"text\" n-i=\"6\" value=\"12\"/><span c:for=\"ivPlayerId\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,short_issue\"/><br/>Iv of the pokemon of your foes<input c:validator=\"short_validator\" id=\"ivFoeId\" name=\"difficulty.ivFoe\" type=\"text\" n-i=\"7\" value=\"4\"/><span c:for=\"ivFoeId\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,short_issue\"/><br/>Rate of won money between winning money while a victory and winning base<input c:validator=\"rate_validator\" id=\"rateWinMoneyBaseId\" name=\"difficulty.rateWinMoneyBase\" type=\"text\" n-i=\"8\" value=\"2\"/><span c:for=\"rateWinMoneyBaseId\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,rate_issue\"/><br/>Rate of lost money between loss while a defeat and winning money while a victory<input c:validator=\"rate_validator\" id=\"rateLooseMoneyWinId\" name=\"difficulty.rateLooseMoneyWin\" type=\"text\" n-i=\"9\" value=\"2\"/><span c:for=\"rateLooseMoneyWinId\" class=\"errormessage\" c:valueMessage=\"msg_difficulty,rate_issue\"/><br/>Healed moves of your pokemon at the end of fight<input name=\"difficulty.restoredMovesEndFight\" type=\"checkbox\" n-i=\"10\"/><br/>The moves wth single target can achieve any foe<input name=\"difficulty.enabledClosing\" type=\"checkbox\" n-i=\"11\" checked=\"checked\"/><br/>Random appearing pokemon<input name=\"difficulty.randomWildFight\" type=\"checkbox\" n-i=\"12\"/><br/>Flee always possible<input name=\"difficulty.stillPossibleFlee\" type=\"checkbox\" n-i=\"13\" checked=\"checked\"/><br/>Do not learnt the already known moves<input name=\"difficulty.skipLearningMovesWhileNotGrowingLevel\" type=\"checkbox\" n-i=\"14\" checked=\"checked\"/><br/>Choice of averages of damage rate for your pokemon<select name=\"difficulty.damageRatePlayer\" n-i=\"15\"><option value=\"CONSTANT_MIN\">M1</option><option value=\"CROISSANT\">M2</option><option value=\"UNIFORME\">M3</option><option value=\"DECROISSANT\">M4</option><option value=\"CONSTANT_MAX\" selected=\"selected\">M5</option></select><br/><table><thead><tr><th>Rate</th><th>Probability</th></tr></thead><tbody><tr><td>1</td><td>1</td></tr></tbody></table>Choix of averages of damage rate for the pokemon of your foes<select name=\"difficulty.damageRateLawFoe\" n-i=\"16\"><option value=\"CONSTANT_MIN\" selected=\"selected\">M1</option><option value=\"CROISSANT\">M2</option><option value=\"UNIFORME\">M3</option><option value=\"DECROISSANT\">M4</option><option value=\"CONSTANT_MAX\">M5</option></select><br/><table><thead><tr><th>Rate</th><th>Probability</th></tr></thead><tbody><tr><td>17/20</td><td>1</td></tr></tbody></table><input value=\"OK\" type=\"submit\"/></form></body></html>",nav_.getHtmlText());
+    }
+
+    private void choose(NatHtmlPage _page, int _nbId, String _value) {
+        _page.getContainer(0, _nbId).setEnabled(true);
+        _page.getContainer(0, _nbId).getNodeInformation().setValue(new StringList(_value));
+    }
 
     private DifficultyCommon result(Struct _str) {
         return ((DifficultyBean) ((PokemonBeanStruct) _str).getInstance()).getDifficultyCommon();
@@ -493,7 +603,10 @@ public final class DifficultyBeanTest extends InitDbBean {
     }
 
     private FacadeGame facTr() {
-        DataBase di_ = diff(2);
+        return facTr(2);
+    }
+    private FacadeGame facTr(int _iv) {
+        DataBase di_ = diff(_iv);
         di_.getTranslatedDiffWinPts().getVal(EN).clear();
         di_.getTranslatedDiffWinPts().getVal(EN).addEntry(DifficultyWinPointsFight.TRES_FACILE,"W1");
         di_.getTranslatedDiffWinPts().getVal(EN).addEntry(DifficultyWinPointsFight.FACILE,"W2");
