e82f1d40eba7f04941a8e853fbdd5ba699632a58
Cardman <f.desrochettes@gmail.com>
2022-10-22 19:15:15 +0200

    prepare tests

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/moves/InitDbMoves.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/moves/InitDbMoves.java
@@ -1,15 +1,24 @@
 package aiki.beans.moves;
 
 import aiki.beans.BeanPokemonCommonTs;
+import aiki.beans.CommonBean;
+import aiki.beans.PokemonBeanStruct;
+import aiki.beans.StringMapObject;
 import aiki.beans.db.InitDbConstr;
 import code.expressionlanguage.structs.Struct;
 
 public abstract class InitDbMoves extends InitDbConstr {
 
-    public static String clickLine(Struct _update, Struct _use, int _index) {
-        fwdLine(_update, _use, _index);
-        beforeDisplaying(_update);
-        return navigateData(clickMoveLineBeanMove(),"","",_update,_index);
+    public static String clickLine(Struct _use, int _index) {
+        MoveLineBean bean_ = new MoveLineBean();
+        CommonBean c_ = (CommonBean) ((PokemonBeanStruct) _use).getBean();
+        bean_.setLanguage(c_.getLanguage());
+        bean_.setDataBase(c_.db());
+        bean_.setForms(new StringMapObject());
+        PokemonBeanStruct moveline_ = new PokemonBeanStruct(bean_);
+        fwdLine(moveline_, _use, _index);
+        beforeDisplaying(moveline_);
+        return navigateData(clickMoveLineBeanMove(),"","",moveline_,_index);
     }
     public static void fwdLine(Struct _update, Struct _use, int _index) {
         callMoveLineBeanIndexSet(_update,_index);
