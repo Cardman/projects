b2f28e7ba2e84d026f03e8fffada2d675fc8eb74
Cardman <f.desrochettes@gmail.com>
2022-10-23 19:03:38 +0200

    prepare tests

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/moves/InitDbMoves.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/moves/InitDbMoves.java
@@ -10,6 +10,82 @@ import code.util.StringMap;
 
 public abstract class InitDbMoves extends InitDbConstr {
 
+    public static Struct callMovesBeanCategorySet(Struct _str, String _args) {
+        return BeanPokemonCommonTs.callString(new MovesBeanCategorySet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMaxAccuracySet(Struct _str, String _args) {
+        return BeanPokemonCommonTs.callString(new MovesBeanMaxAccuracySet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMaxPowerSet(Struct _str, String _args) {
+        return BeanPokemonCommonTs.callString(new MovesBeanMaxPowerSet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMinAccuracySet(Struct _str, String _args) {
+        return BeanPokemonCommonTs.callString(new MovesBeanMinAccuracySet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMinPowerSet(Struct _str, String _args) {
+        return BeanPokemonCommonTs.callString(new MovesBeanMinPowerSet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanTypedNameSet(Struct _str, String _args) {
+        return BeanPokemonCommonTs.callString(new MovesBeanTypedNameSet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanTypedTypeSet(Struct _str, String _args) {
+        return BeanPokemonCommonTs.callString(new MovesBeanTypedTypeSet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanWholeWordSet(Struct _str, boolean _args) {
+        return BeanPokemonCommonTs.callBool(new MovesBeanWholeWordSet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanCategoriesGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanCategoriesGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanCategoryGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanCategoryGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMaxAccuracyGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanMaxAccuracyGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMaxPowerGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanMaxPowerGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMinAccuracyGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanMinAccuracyGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMinPowerGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanMinPowerGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanMovesGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanMovesGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanSearch(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanSearch(),_str,_args);
+    }
+
+    public static Struct callMovesBeanTypedNameGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanTypedNameGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanTypedTypeGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanTypedTypeGet(),_str,_args);
+    }
+
+    public static Struct callMovesBeanWholeWordGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new MovesBeanWholeWordGet(),_str,_args);
+    }
+
     public static String clickLine(Struct _use, StringMap<Struct> _all, int _index) {
         MoveLineBean bean_ = new MoveLineBean();
         CommonBean c_ = (CommonBean) ((PokemonBeanStruct) _use).getBean();
