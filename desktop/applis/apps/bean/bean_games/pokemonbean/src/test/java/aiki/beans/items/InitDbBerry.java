df1e71e42185d8bb1b3dbbb233489b34d3fc4c2d
Cardman <f.desrochettes@gmail.com>
2022-11-05 09:32:30 +0100

    split initializers

---
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/items/InitDbBerry.java
@@ -0,0 +1,92 @@
+package aiki.beans.items;
+
+import aiki.beans.*;
+import code.expressionlanguage.structs.Struct;
+
+public abstract class InitDbBerry extends InitDbItem {
+
+    public static Struct callBerryBeanBoostStatisGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanBoostStatisGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanCategoryBoostingGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanCategoryBoostingGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanClickStatus(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanClickStatus(),_str,_args);
+    }
+
+    public static Struct callBerryBeanDamageRateRecoilFoeGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanDamageRateRecoilFoeGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanGetTrBoostStat(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrBoostStat(),_str,_args);
+    }
+
+    public static Struct callBerryBeanGetTrCategRecoil(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrCategRecoil(),_str,_args);
+    }
+
+    public static Struct callBerryBeanGetTrMultFoesDamage(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrMultFoesDamage(),_str,_args);
+    }
+
+    public static Struct callBerryBeanGetTrMultStat(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrMultStat(),_str,_args);
+    }
+
+    public static Struct callBerryBeanGetTrStatus(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrStatus(),_str,_args);
+    }
+
+    public static Struct callBerryBeanHealHpBySuperEffMoveGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanHealHpBySuperEffMoveGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanHealHpGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanHealHpGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanHealHpRateGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanHealHpRateGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanHealPpGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanHealPpGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanHealStatusGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanHealStatusGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanIsHealingPp(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanIsHealingPp(),_str,_args);
+    }
+
+    public static Struct callBerryBeanLawForAttackFirstGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanLawForAttackFirstGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanMaxHpHealingHpGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanMaxHpHealingHpGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanMaxHpHealingHpRateGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanMaxHpHealingHpRateGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanMultFoesDamageGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanMultFoesDamageGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanMultStatGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanMultStatGet(),_str,_args);
+    }
+
+    public static Struct callBerryBeanWithoutFailGet(Struct _str, long... _args) {
+        return BeanPokemonCommonTs.callLongs(new BerryBeanWithoutFailGet(),_str,_args);
+    }
+
+}
--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/items/InitDbItemOther.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/items/InitDbItemOther.java
@@ -4,13 +4,10 @@ import aiki.beans.BeanPokemonCommonTs;
 import aiki.db.DataBase;
 import aiki.facade.FacadeGame;
 import aiki.fight.moves.enums.TargetChoice;
-import aiki.fight.pokemon.enums.GenderRepartition;
-import aiki.instances.Instances;
 import code.expressionlanguage.structs.Struct;
 import code.images.BaseSixtyFourUtil;
 import code.maths.Rate;
-import code.util.StringList;
-import code.util.StringMap;
+import code.util.*;
 
 public abstract class InitDbItemOther extends InitDbItem {
 
@@ -21,90 +18,6 @@ public abstract class InitDbItemOther extends InitDbItem {
 
     public static Struct callBallBeanMapVarsGet(Struct _str, long... _args) {
         return BeanPokemonCommonTs.callLongs(new BallBeanMapVarsGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanBoostStatisGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanBoostStatisGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanCategoryBoostingGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanCategoryBoostingGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanClickStatus(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanClickStatus(),_str,_args);
-    }
-
-    public static Struct callBerryBeanDamageRateRecoilFoeGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanDamageRateRecoilFoeGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanGetTrBoostStat(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrBoostStat(),_str,_args);
-    }
-
-    public static Struct callBerryBeanGetTrCategRecoil(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrCategRecoil(),_str,_args);
-    }
-
-    public static Struct callBerryBeanGetTrMultFoesDamage(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrMultFoesDamage(),_str,_args);
-    }
-
-    public static Struct callBerryBeanGetTrMultStat(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrMultStat(),_str,_args);
-    }
-
-    public static Struct callBerryBeanGetTrStatus(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanGetTrStatus(),_str,_args);
-    }
-
-    public static Struct callBerryBeanHealHpBySuperEffMoveGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanHealHpBySuperEffMoveGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanHealHpGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanHealHpGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanHealHpRateGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanHealHpRateGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanHealPpGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanHealPpGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanHealStatusGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanHealStatusGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanIsHealingPp(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanIsHealingPp(),_str,_args);
-    }
-
-    public static Struct callBerryBeanLawForAttackFirstGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanLawForAttackFirstGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanMaxHpHealingHpGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanMaxHpHealingHpGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanMaxHpHealingHpRateGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanMaxHpHealingHpRateGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanMultFoesDamageGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanMultFoesDamageGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanMultStatGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanMultStatGet(),_str,_args);
-    }
-
-    public static Struct callBerryBeanWithoutFailGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new BerryBeanWithoutFailGet(),_str,_args);
     }
 
     public static String callBoostBeanClickHappiness(Struct _str, long... _args) {
