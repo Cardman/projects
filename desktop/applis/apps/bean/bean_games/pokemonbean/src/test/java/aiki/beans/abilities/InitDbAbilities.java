78ba07369fbf1256ade555ee3cbf32ebbed7d787
Cardman <f.desrochettes@gmail.com>
2022-11-10 13:16:03 +0100

    prepare tests

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/abilities/InitDbAbilities.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/abilities/InitDbAbilities.java
@@ -2,13 +2,17 @@ package aiki.beans.abilities;
 
 import aiki.beans.*;
 import aiki.beans.db.InitDbConstr;
+import aiki.db.DataBase;
 import aiki.facade.FacadeGame;
 import aiki.facade.enums.SelectedBoolean;
+import aiki.fight.abilities.AbilityData;
 import aiki.fight.enums.Statistic;
+import aiki.fight.util.WeatherType;
+import aiki.game.fight.Fight;
 import aiki.instances.Instances;
 import code.expressionlanguage.structs.Struct;
-import code.util.IdMap;
-import code.util.StringMap;
+import code.maths.*;
+import code.util.*;
 
 public abstract class InitDbAbilities extends InitDbConstr {
 
@@ -137,5 +141,82 @@ public abstract class InitDbAbilities extends InitDbConstr {
         _facade.getData().getTranslatedBooleans().getVal(EN).addEntry(SelectedBoolean.NO, B_NO);
         _facade.getData().getTranslatedBooleans().getVal(EN).addEntry(SelectedBoolean.YES, B_YES);
         _facade.getData().getTranslatedBooleans().getVal(EN).addEntry(SelectedBoolean.YES_AND_NO," ");
+        _facade.getData().getLitterals().addEntry(EN,new StringMap<String>());
+        _facade.getData().getLitterals().getVal(EN).addEntry(Fight.TEMPS_TOUR, TAB+Fight.TEMPS_TOUR+TAB+TIME);
+    }
+    protected static AbilityData ability(boolean _forbidUseBerryAgainstFoes, boolean _chgtTypeByDamage, boolean _ignFoeStatisBoost, boolean _immuCh, boolean _immuDamageTrappingMoves, boolean _immuDamageAllyMoves, boolean _immuDamageRecoil, boolean _copyMovesTypes, boolean _reverseEffectsPowerMovesTypesGlobal, boolean _takeItemByDamagingMove, boolean _giveItemToAllyHavingUsed, boolean _inflictingDamageInsteadOfSuffering, boolean _nbHits, boolean _breakProtection, boolean _plate, boolean _healedStatusBySwitch, boolean _achievedDisappearedPk, boolean _mumy, boolean _immuRechargeRound, boolean _slowing, boolean _immuSufferedDamageLowEff, boolean _cancelSecEffectOther, boolean _cancelSecEffectOwner, int _nbUsedPp, int _decreaseNecStepsHatch, String _typeForMoves){
+        AbilityData a_ = Instances.newAbilityData();
+        a_.setForbidUseBerryAgainstFoes(_forbidUseBerryAgainstFoes);
+        a_.setChgtTypeByDamage(_chgtTypeByDamage);
+        a_.setIgnFoeStatisBoost(_ignFoeStatisBoost);
+        a_.setImmuCh(_immuCh);
+        a_.setImmuDamageTrappingMoves(_immuDamageTrappingMoves);
+        a_.setImmuDamageAllyMoves(_immuDamageAllyMoves);
+        a_.setImmuDamageRecoil(_immuDamageRecoil);
+
+        a_.setCopyMovesTypes(_copyMovesTypes);
+
+        a_.setReverseEffectsPowerMovesTypesGlobal(_reverseEffectsPowerMovesTypesGlobal);
+
+        a_.setTakeItemByDamagingMove(_takeItemByDamagingMove);
+
+        a_.setGiveItemToAllyHavingUsed(_giveItemToAllyHavingUsed);
+        a_.setInflictingDamageInsteadOfSuffering(_inflictingDamageInsteadOfSuffering);
+        a_.setNbHits(_nbHits);
+        a_.setBreakProtection(_breakProtection);
+        a_.setPlate(_plate);
+        a_.setHealedStatusBySwitch(_healedStatusBySwitch);
+        a_.setAchievedDisappearedPk(_achievedDisappearedPk);
+        a_.setMumy(_mumy);
+        a_.setImmuRechargeRound(_immuRechargeRound);
+        a_.setSlowing(_slowing);
+        a_.setImmuSufferedDamageLowEff(_immuSufferedDamageLowEff);
+        a_.setCancelSecEffectOther(_cancelSecEffectOther);
+        a_.setCancelSecEffectOwner(_cancelSecEffectOwner);
+
+
+        a_.setNbUsedPp(_nbUsedPp);
+        a_.setDecreaseNecStepsHatch(_decreaseNecStepsHatch);
+
+        a_.setTypeForMoves(_typeForMoves);
+        a_.setRecoilDamageFoe(Rate.one());
+        a_.setMultStab(Rate.one());
+        a_.setMultEvtRateSecEffectOwner(Rate.one());
+        a_.setMultVarBoost(Rate.one());
+        a_.setMultAllyDamage(Rate.one());
+        a_.setMultEvtRateCh(Rate.one());
+        a_.setMultDamageCh(Rate.one());
+        a_.setMultSufferedDamageSuperEff(Rate.one());
+        a_.setMaxHpForUsingBerry(Rate.one());
+        a_.setHealedHpRateBySwitch(Rate.one());
+        a_.setHealHpWhileUsingBerry(Rate.one());
+        a_.setMultDamage(DataBase.VAR_PREFIX+Fight.TEMPS_TOUR);
+        a_.setMultPower(DataBase.VAR_PREFIX+Fight.TEMPS_TOUR);
+        a_.getBonusStatRank().addEntry(Statistic.SPEED,(byte)1);
+        a_.getBoostStatRankEndRound().addEntry(Statistic.SPEED,(byte)1);
+        a_.getBoostStatRankProtected().addEntry(Statistic.SPEED,(byte)1);
+        a_.getMultStat().addEntry(Statistic.SPEED,DataBase.VAR_PREFIX+Fight.TEMPS_TOUR);
+        a_.getImmuMoveTypesByWeather().addEntry(NULL_REF,new StringList());
+        a_.getImmuMoveTypesByWeather().addEntry(M_DAM,new StringList(T_TYPE1));
+        a_.getChgtTypeByWeather().addEntry(NULL_REF,T_TYPE1);
+        a_.getChgtTypeByWeather().addEntry(M_DAM,T_TYPE2);
+        a_.getImmuStatus().addEntry(NULL_REF,new StringList());
+        a_.getImmuStatus().addEntry(M_DAM,new StringList(S_STA_SIM));
+        a_.getHealHpByWeather().addEntry(NULL_REF,Rate.one());
+        a_.getHealHpByWeather().addEntry(M_DAM,Rate.newRate("2"));
+        a_.getHealHpByTypeIfWeather().addEntry(new WeatherType(NULL_REF,T_TYPE1),Rate.one());
+        a_.getHealHpByTypeIfWeather().addEntry(new WeatherType(NULL_REF,T_TYPE1),Rate.newRate("2"));
+        a_.getImmuStatusBeginRound().add(S_STA_SIM);
+        a_.getImmuMove().add(M_DAM);
+        a_.getIgnFoeTeamMove().add(M_DAM);
+        a_.getImmuAllyFromMoves().add(M_DAM);
+        a_.getIgnAbility().add(A_ABILITY2);
+        a_.getImmuAbility().add(A_ABILITY2);
+        a_.getImmuStatus().addEntry(M_DAM,new StringList(S_STA_SIM));
+        a_.getImmuWeather().add(M_DAM);
+        a_.getSingleStatus().addQuickEvent(NULL_REF, LgInt.newLgInt("1"));
+        a_.getSingleStatus().addQuickEvent(S_STA_SIM,LgInt.newLgInt("3"));
+        a_.getFailStatus().addEntry(S_STA_SIM,DataBase.VAR_PREFIX+Fight.TEMPS_TOUR);
+        return a_;
     }
 }
