b7eedebb37b5d1987cd93f5b9bfde16cfb024d52
Cardman <f.desrochettes@gmail.com>
2022-11-12 16:32:46 +0100

    refactor code

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/main/java/aiki/beans/effects/ComboDto.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/main/java/aiki/beans/effects/ComboDto.java
@@ -16,24 +16,8 @@ public final class ComboDto {
         return combos.getValue(_index);
     }
 
-    public boolean isEmpty() {
-        return combos.isEmpty();
-    }
-
     public StringList getKey(int _index) {
         return combos.getKey(_index);
-    }
-
-    public int size() {
-        return combos.size();
-    }
-
-    public void put(StringList _key, EffectCombo _value) {
-        combos.put(_key, _value);
-    }
-
-    public EffectCombo getVal(StringList _key) {
-        return combos.getVal(_key);
     }
 
 }
