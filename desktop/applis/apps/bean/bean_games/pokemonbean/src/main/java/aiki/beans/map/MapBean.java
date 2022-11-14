97ad17dc5171c72c67041f1d88b54103e6eb1a01
Cardman <f.desrochettes@gmail.com>
2022-11-14 16:13:35 +0100

    refactor code

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/main/java/aiki/beans/map/MapBean.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/main/java/aiki/beans/map/MapBean.java
@@ -2,11 +2,7 @@ package aiki.beans.map;
 
 import aiki.beans.CommonBean;
 import aiki.beans.facade.map.dto.PlaceIndex;
-import aiki.map.buildings.Building;
-import aiki.map.buildings.Gym;
-import aiki.map.buildings.PokemonCenter;
 import aiki.map.levels.Level;
-import aiki.map.places.City;
 import aiki.map.places.Place;
 import code.util.CustList;
 import code.util.core.IndexConstants;
@@ -26,26 +22,9 @@ public class MapBean extends CommonBean {
         Place pl_ = places.get(_index).getPlace();
         return pl_.getLevelsList();
     }
-    public boolean isCenter(int _indexOne, int _indexTwo) {
-        return buildings(_indexOne).get(_indexTwo) instanceof PokemonCenter;
-    }
-    public boolean isGym(int _indexOne, int _indexTwo) {
-        return buildings(_indexOne).get(_indexTwo) instanceof Gym;
-    }
-    public CustList<Building> buildings(int _index) {
-        Place pl_ = places.get(_index).getPlace();
-        if (!(pl_ instanceof City)) {
-            return new CustList<Building>();
-        }
-        City c_ = (City) pl_;
-        return c_.getBuildings().values();
-    }
-    public boolean isCity(int _index) {
-        return places.get(_index).getPlace() instanceof City;
-    }
     public String clickLevel(int _indexOne, int _indexTwo) {
         feedForms(_indexOne, _indexTwo, getForms());
-        return CST_LEVEL;
+        return AikiBeansMapStd.WEB_HTML_MAP_LEVEL_HTML;
     }
 
     public CustList<PlaceIndex> getPlaces() {
