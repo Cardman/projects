7a49db9c99883463ebebb455204539290df545dc
Cardman <f.desrochettes@gmail.com>
2022-11-18 23:22:24 +0100

    add tests

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/help/InitDbLangs.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/help/InitDbLangs.java
@@ -1,129 +1,303 @@
 package aiki.beans.help;
 
-import aiki.beans.BeanPokemonCommonTs;
-import aiki.beans.db.InitDbConstr;
+import aiki.beans.*;
+import aiki.beans.db.*;
+import aiki.db.*;
+import aiki.facade.*;
+import aiki.facade.enums.*;
+import aiki.fight.enums.*;
+import aiki.fight.moves.enums.*;
+import aiki.instances.*;
+import aiki.map.levels.enums.*;
+import aiki.map.pokemon.enums.*;
 import code.expressionlanguage.structs.Struct;
+import code.util.*;
 
 public abstract class InitDbLangs extends InitDbConstr {
+    public static final String FR = "fr";
+    public static final String C_CA = "C_CA";
+    public static final String T_T = "T_T";
 
-    public static Struct callLangsBeanGetKeysAbilities(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysAbilities(),_str,_args);
+    public static Struct callLangsBeanGetKeysAbilities() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysAbilities(),str());
     }
 
-    public static Struct callLangsBeanGetKeysBooleans(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysBooleans(),_str,_args);
+    public static Struct callLangsBeanGetKeysBooleans() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysBooleans(),str());
     }
 
-    public static Struct callLangsBeanGetKeysCategories(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysCategories(),_str,_args);
+    public static Struct callLangsBeanGetKeysCategories() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysCategories(),str());
     }
 
-    public static Struct callLangsBeanGetKeysDesc(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysDesc(),_str,_args);
+    public static Struct callLangsBeanGetKeysDesc() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysDesc(),str());
     }
 
-    public static Struct callLangsBeanGetKeysEnvironments(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysEnvironments(),_str,_args);
+    public static Struct callLangsBeanGetKeysEnvironments() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysEnvironments(),str());
     }
 
-    public static Struct callLangsBeanGetKeysGenders(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysGenders(),_str,_args);
+    public static Struct callLangsBeanGetKeysGenders() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysGenders(),str());
     }
 
-    public static Struct callLangsBeanGetKeysItems(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysItems(),_str,_args);
+    public static Struct callLangsBeanGetKeysItems() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysItems(),str());
     }
 
-    public static Struct callLangsBeanGetKeysMath(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysMath(),_str,_args);
+    public static Struct callLangsBeanGetKeysMath() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysMath(),str());
     }
 
-    public static Struct callLangsBeanGetKeysMoves(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysMoves(),_str,_args);
+    public static Struct callLangsBeanGetKeysMoves() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysMoves(),str());
     }
 
-    public static Struct callLangsBeanGetKeysPokemon(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysPokemon(),_str,_args);
+    public static Struct callLangsBeanGetKeysPokemon() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysPokemon(),str());
     }
 
-    public static Struct callLangsBeanGetKeysStatistics(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysStatistics(),_str,_args);
+    public static Struct callLangsBeanGetKeysStatistics() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysStatistics(),str());
     }
 
-    public static Struct callLangsBeanGetKeysStatus(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysStatus(),_str,_args);
+    public static Struct callLangsBeanGetKeysStatus() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysStatus(),str());
     }
 
-    public static Struct callLangsBeanGetKeysTargets(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysTargets(),_str,_args);
+    public static Struct callLangsBeanGetKeysTargets() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysTargets(),str());
     }
 
-    public static Struct callLangsBeanGetKeysTypes(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysTypes(),_str,_args);
+    public static Struct callLangsBeanGetKeysTypes() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetKeysTypes(),str());
     }
 
-    public static Struct callLangsBeanGetRowAbility(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowAbility(),_str,_args);
+    public static Struct callLangsBeanGetRowAbility(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowAbility(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowBoolean(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowBoolean(),_str,_args);
+    public static Struct callLangsBeanGetRowBoolean(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowBoolean(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowCategory(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowCategory(),_str,_args);
+    public static Struct callLangsBeanGetRowCategory(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowCategory(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowDesc(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowDesc(),_str,_args);
+    public static Struct callLangsBeanGetRowDesc(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowDesc(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowEnvironment(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowEnvironment(),_str,_args);
+    public static Struct callLangsBeanGetRowEnvironment(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowEnvironment(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowGender(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowGender(),_str,_args);
+    public static Struct callLangsBeanGetRowGender(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowGender(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowItem(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowItem(),_str,_args);
+    public static Struct callLangsBeanGetRowItem(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowItem(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowMath(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowMath(),_str,_args);
+    public static Struct callLangsBeanGetRowMath(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowMath(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowMove(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowMove(),_str,_args);
+    public static Struct callLangsBeanGetRowMove(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowMove(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowPokemon(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowPokemon(),_str,_args);
+    public static Struct callLangsBeanGetRowPokemon(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowPokemon(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowStatistic(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowStatistic(),_str,_args);
+    public static Struct callLangsBeanGetRowStatistic(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowStatistic(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowStatus(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowStatus(),_str,_args);
+    public static Struct callLangsBeanGetRowStatus(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowStatus(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowTarget(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowTarget(),_str,_args);
+    public static Struct callLangsBeanGetRowTarget(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowTarget(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetRowType(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowType(),_str,_args);
+    public static Struct callLangsBeanGetRowType(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetRowType(),str(),_args);
     }
 
-    public static Struct callLangsBeanGetTrLang(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanGetTrLang(),_str,_args);
+    public static Struct callLangsBeanGetTrLang(int _args) {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanGetTrLang(),str(),_args);
     }
 
-    public static Struct callLangsBeanLanguagesGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LangsBeanLanguagesGet(),_str,_args);
+    public static Struct callLangsBeanLanguagesGet() {
+        return BeanPokemonCommonTs.callLongs(new LangsBeanLanguagesGet(),str());
+    }
+    protected static Struct str() {
+        PkData pk_ = pkDataByFacade(db());
+        PokemonBeanStruct bean_ = pk_.beanLangsBean(EN);
+        beforeDisplaying(bean_);
+        return bean_;
+    }
+    protected static FacadeGame db(){
+        DataBase data_ = newData();
+        data_.setLanguage(EN);
+        data_.setLanguages(new StringList(EN,FR));
+        data_.initializeMembers();
+        data_.initValue(DataBase.DEF_CAT,AUTRE);
+        FacadeGame fac_ = new FacadeGame();
+        fac_.setLanguages(new StringList(EN,FR));
+        StringMap<String> displayLanguages_ = new StringMap<String>();
+        displayLanguages_.addEntry(EN,"EN1");
+        displayLanguages_.addEntry(FR,"FR2");
+        fac_.setDisplayLanguages(displayLanguages_);
+        data_.setDisplayLanguages(displayLanguages_);
+        fac_.setData(data_);
+        data_.setMessages(fac_.getData());
+        fac_.setLoadedData(true);
+        fac_.setZipName("");
+        fac_.setData(data_);
+        fac_.setLanguage(EN);
+        data_.getTypes().add(T_T);
+        data_.completeMembers(P_POKEMON,Instances.newPokemonData());
+        data_.completeMembers(I_ITEM,Instances.newBall());
+        data_.completeMembers(M_DAM,Instances.newDamagingMoveData());
+        data_.completeMembers(A_ABILITY,Instances.newAbilityData());
+        data_.completeMembers(S_STA_SIM,Instances.newStatusSimple());
+        firstLg(data_);
+        secLg(data_);
+        data_.getAllCategories().clear();
+        data_.getAllCategories().add(C_CA);
+        return fac_;
     }
 
+    private static void secLg(DataBase _data) {
+        IdMap<Gender, String> gdrs2_ = new IdMap<Gender, String>();
+        gdrs2_.addEntry(Gender.NO_GENDER, "NO_G2");
+        gdrs2_.addEntry(Gender.FEMALE,"FE2");
+        gdrs2_.addEntry(Gender.MALE,"MA2");
+        _data.getTranslatedGenders().addEntry(FR, gdrs2_);
+        IdMap<SelectedBoolean, String> bools2_ = new IdMap<SelectedBoolean, String>();
+        bools2_.addEntry(SelectedBoolean.NO,"NO2");
+        bools2_.addEntry(SelectedBoolean.YES,"YES2");
+        bools2_.addEntry(SelectedBoolean.YES_AND_NO,"YES_AND_NO2");
+        _data.getTranslatedBooleans().addEntry(FR, bools2_);
+        IdMap<EnvironmentType, String> et2_ = new IdMap<EnvironmentType, String>();
+        et2_.addEntry(EnvironmentType.ROAD,"ROAD2");
+        et2_.addEntry(EnvironmentType.ROCK,"ROCK2");
+        et2_.addEntry(EnvironmentType.NOTHING,"NOTHING2");
+        et2_.addEntry(EnvironmentType.BUILDING,"BUILDING2");
+        et2_.addEntry(EnvironmentType.GRASS,"GRASS2");
+        et2_.addEntry(EnvironmentType.DESERT,"DESERT2");
+        et2_.addEntry(EnvironmentType.ICE,"ICE2");
+        et2_.addEntry(EnvironmentType.SNOW,"SNOW2");
+        et2_.addEntry(EnvironmentType.WATER,"WATER2");
+        _data.getTranslatedEnvironment().addEntry(FR, et2_);
+        IdMap<Statistic, String> stats2_ = new IdMap<Statistic, String>();
+        stats2_.addEntry(Statistic.ATTACK,"ATTACK2");
+        stats2_.addEntry(Statistic.SPECIAL_ATTACK,"SPECIAL_ATTACK2");
+        stats2_.addEntry(Statistic.DEFENSE,"DEFENSE2");
+        stats2_.addEntry(Statistic.SPECIAL_DEFENSE,"SPECIAL_DEFENSE2");
+        stats2_.addEntry(Statistic.SPEED, "SPEED2");
+        stats2_.addEntry(Statistic.ACCURACY,"ACCURACY2");
+        stats2_.addEntry(Statistic.EVASINESS,"EVASINESS2");
+        stats2_.addEntry(Statistic.HP,"HP2");
+        stats2_.addEntry(Statistic.PV_RESTANTS,"PV_RESTANTS2");
+        stats2_.addEntry(Statistic.CRITICAL_HIT,"CRITICAL_HIT2");
+        _data.getTranslatedStatistics().addEntry(FR, stats2_);
+        IdMap<TargetChoice, String> tar2_ = new IdMap<TargetChoice, String>();
+        tar2_.addEntry(TargetChoice.ADJ_ADV,"ADJ_ADV2");
+        tar2_.addEntry(TargetChoice.ADJ_MULT,"ADJ_MULT2");
+        tar2_.addEntry(TargetChoice.ANY_FOE,"ANY_FOE2");
+        tar2_.addEntry(TargetChoice.ALLIE,"ALLIE2");
+        tar2_.addEntry(TargetChoice.ALLIES,"ALLIES2");
+        tar2_.addEntry(TargetChoice.AUTRE_UNIQ,"AUTRE_UNIQ2");
+        tar2_.addEntry(TargetChoice.ADJ_UNIQ,"ADJ_UNIQ2");
+        tar2_.addEntry(TargetChoice.GLOBALE,"GLOBALE2");
+        tar2_.addEntry(TargetChoice.LANCEUR,"LANCEUR2");
+        tar2_.addEntry(TargetChoice.NOTHING,"NOTHING2");
+        tar2_.addEntry(TargetChoice.TOUS_ADV,"TOUS_ADV2");
+        tar2_.addEntry(TargetChoice.PSEUDO_GLOBALE,"PSEUDO_GLOBALE2");
+        tar2_.addEntry(TargetChoice.UNIQUE_IMPORTE,"UNIQUE_IMPORTE2");
+        _data.getTranslatedTargets().addEntry(FR, tar2_);
+        _data.getTranslatedCategories().addEntry(FR,  withTr(C_CA, "C_CA2"));
+        _data.getTranslatedTypes().addEntry(FR, withTr(T_T,"T_T2"));
+        _data.getTranslatedPokemon().addEntry(FR, withTr(P_POKEMON,"P_POKEMON2"));
+        _data.getTranslatedItems().addEntry(FR, withTr(I_ITEM,"I_ITEM2"));
+        _data.getTranslatedClassesDescriptions().addEntry(FR, withTr(_data.getItem(I_ITEM).getItemType(),"I_ITEM_2"));
+        _data.getTranslatedMoves().addEntry(FR, withTr(M_DAM,"M_DAM2"));
+        _data.getTranslatedAbilities().addEntry(FR, withTr(A_ABILITY,"A_ABILITY2"));
+        _data.getTranslatedStatus().addEntry(FR, withTr(S_STA_SIM,"S_STA_SIM2"));
+        _data.getTranslatedFctMath().addEntry(FR, withTr("lg","lg2"));
+    }
+
+    private static void firstLg(DataBase _data) {
+        IdMap<Gender, String> gdrs_ = new IdMap<Gender, String>();
+        gdrs_.addEntry(Gender.NO_GENDER, "NO_G1");
+        gdrs_.addEntry(Gender.FEMALE,"FE1");
+        gdrs_.addEntry(Gender.MALE,"MA1");
+        _data.getTranslatedGenders().addEntry(EN, gdrs_);
+        IdMap<SelectedBoolean, String> bools_ = new IdMap<SelectedBoolean, String>();
+        bools_.addEntry(SelectedBoolean.NO,"NO1");
+        bools_.addEntry(SelectedBoolean.YES,"YES1");
+        bools_.addEntry(SelectedBoolean.YES_AND_NO,"YES_AND_NO1");
+        _data.getTranslatedBooleans().addEntry(EN, bools_);
+        IdMap<EnvironmentType, String> et_ = new IdMap<EnvironmentType, String>();
+        et_.addEntry(EnvironmentType.ROAD,"ROAD1");
+        et_.addEntry(EnvironmentType.ROCK,"ROCK1");
+        et_.addEntry(EnvironmentType.NOTHING,"NOTHING1");
+        et_.addEntry(EnvironmentType.BUILDING,"BUILDING1");
+        et_.addEntry(EnvironmentType.GRASS,"GRASS1");
+        et_.addEntry(EnvironmentType.DESERT,"DESERT1");
+        et_.addEntry(EnvironmentType.ICE,"ICE1");
+        et_.addEntry(EnvironmentType.SNOW,"SNOW1");
+        et_.addEntry(EnvironmentType.WATER,"WATER1");
+        _data.getTranslatedEnvironment().addEntry(EN, et_);
+        IdMap<Statistic, String> stats_ = new IdMap<Statistic, String>();
+        stats_.addEntry(Statistic.ATTACK,"ATTACK1");
+        stats_.addEntry(Statistic.SPECIAL_ATTACK,"SPECIAL_ATTACK1");
+        stats_.addEntry(Statistic.DEFENSE,"DEFENSE1");
+        stats_.addEntry(Statistic.SPECIAL_DEFENSE,"SPECIAL_DEFENSE1");
+        stats_.addEntry(Statistic.SPEED, "SPEED1");
+        stats_.addEntry(Statistic.ACCURACY,"ACCURACY1");
+        stats_.addEntry(Statistic.EVASINESS,"EVASINESS1");
+        stats_.addEntry(Statistic.HP,"HP1");
+        stats_.addEntry(Statistic.PV_RESTANTS,"PV_RESTANTS1");
+        stats_.addEntry(Statistic.CRITICAL_HIT,"CRITICAL_HIT1");
+        _data.getTranslatedStatistics().addEntry(EN, stats_);
+        IdMap<TargetChoice, String> tar_ = new IdMap<TargetChoice, String>();
+        tar_.addEntry(TargetChoice.ADJ_ADV,"ADJ_ADV1");
+        tar_.addEntry(TargetChoice.ADJ_MULT,"ADJ_MULT1");
+        tar_.addEntry(TargetChoice.ANY_FOE,"ANY_FOE1");
+        tar_.addEntry(TargetChoice.ALLIE,"ALLIE1");
+        tar_.addEntry(TargetChoice.ALLIES,"ALLIES1");
+        tar_.addEntry(TargetChoice.AUTRE_UNIQ,"AUTRE_UNIQ1");
+        tar_.addEntry(TargetChoice.ADJ_UNIQ,"ADJ_UNIQ1");
+        tar_.addEntry(TargetChoice.GLOBALE,"GLOBALE1");
+        tar_.addEntry(TargetChoice.LANCEUR,"LANCEUR1");
+        tar_.addEntry(TargetChoice.NOTHING,"NOTHING1");
+        tar_.addEntry(TargetChoice.TOUS_ADV,"TOUS_ADV1");
+        tar_.addEntry(TargetChoice.PSEUDO_GLOBALE,"PSEUDO_GLOBALE1");
+        tar_.addEntry(TargetChoice.UNIQUE_IMPORTE,"UNIQUE_IMPORTE1");
+        _data.getTranslatedTargets().addEntry(EN, tar_);
+        _data.getTranslatedCategories().addEntry(EN, withTr(C_CA, "C_CA1"));
+        _data.getTranslatedTypes().addEntry(EN, withTr(T_T,"T_T1"));
+        _data.getTranslatedPokemon().addEntry(EN, withTr(P_POKEMON,"P_POKEMON1"));
+        _data.getTranslatedItems().addEntry(EN, withTr(I_ITEM,"I_ITEM1"));
+        _data.getTranslatedClassesDescriptions().addEntry(EN, withTr(_data.getItem(I_ITEM).getItemType(),"I_ITEM_1"));
+        _data.getTranslatedMoves().addEntry(EN, withTr(M_DAM,"M_DAM1"));
+        _data.getTranslatedAbilities().addEntry(EN, withTr(A_ABILITY,"A_ABILITY1"));
+        _data.getTranslatedStatus().addEntry(EN, withTr(S_STA_SIM,"S_STA_SIM1"));
+        _data.getTranslatedFctMath().addEntry(EN, withTr("lg","lg1"));
+    }
+
+    private static StringMap<String> withTr(String _key, String _value) {
+        StringMap<String> cat_ = new StringMap<String>();
+        cat_.addEntry(_key, _value);
+        return cat_;
+    }
 }
---
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/help/LangsBeanTest.java
@@ -0,0 +1,226 @@
+package aiki.beans.help;
+
+import org.junit.Test;
+
+public final class LangsBeanTest extends InitDbLangs {
+    @Test
+    public void getLanguages1() {
+        assertSizeEq(2,callLangsBeanLanguagesGet());
+    }
+    @Test
+    public void getLanguages2() {
+        assertEq(EN,elt(callLangsBeanLanguagesGet(),0));
+    }
+    @Test
+    public void getLanguages3() {
+        assertEq(FR,elt(callLangsBeanLanguagesGet(),1));
+    }
+    @Test
+    public void getTrLang1() {
+        assertEq("EN1",callLangsBeanGetTrLang(0));
+    }
+    @Test
+    public void getTrLang2() {
+        assertEq("FR2",callLangsBeanGetTrLang(1));
+    }
+    @Test
+    public void getKeysGenders() {
+        assertSizeEq(3,callLangsBeanGetKeysGenders());
+    }
+    @Test
+    public void getRowGender1() {
+        assertEq("FE1",elt(callLangsBeanGetRowGender(0),0));
+    }
+    @Test
+    public void getRowGender2() {
+        assertEq("MA1",elt(callLangsBeanGetRowGender(1),0));
+    }
+    @Test
+    public void getRowGender3() {
+        assertEq("NO_G1",elt(callLangsBeanGetRowGender(2),0));
+    }
+    @Test
+    public void getRowGender4() {
+        assertEq("FE2",elt(callLangsBeanGetRowGender(0),1));
+    }
+    @Test
+    public void getRowGender5() {
+        assertEq("MA2",elt(callLangsBeanGetRowGender(1),1));
+    }
+    @Test
+    public void getRowGender6() {
+        assertEq("NO_G2",elt(callLangsBeanGetRowGender(2),1));
+    }
+    @Test
+    public void getKeysBooleans() {
+        assertSizeEq(3,callLangsBeanGetKeysBooleans());
+    }
+    @Test
+    public void getRowBoolean1() {
+        assertEq("NO1",elt(callLangsBeanGetRowBoolean(0),0));
+    }
+    @Test
+    public void getRowBoolean2() {
+        assertEq("YES1",elt(callLangsBeanGetRowBoolean(1),0));
+    }
+    @Test
+    public void getRowBoolean3() {
+        assertEq("YES_AND_NO1",elt(callLangsBeanGetRowBoolean(2),0));
+    }
+    @Test
+    public void getRowBoolean4() {
+        assertEq("NO2",elt(callLangsBeanGetRowBoolean(0),1));
+    }
+    @Test
+    public void getRowBoolean5() {
+        assertEq("YES2",elt(callLangsBeanGetRowBoolean(1),1));
+    }
+    @Test
+    public void getRowBoolean6() {
+        assertEq("YES_AND_NO2",elt(callLangsBeanGetRowBoolean(2),1));
+    }
+    @Test
+    public void getKeysEnvironments() {
+        assertSizeEq(9,callLangsBeanGetKeysEnvironments());
+    }
+    @Test
+    public void getRowEnvironment1() {
+        assertEq("BUILDING1",elt(callLangsBeanGetRowEnvironment(0),0));
+    }
+    @Test
+    public void getRowEnvironment2() {
+        assertEq("BUILDING2",elt(callLangsBeanGetRowEnvironment(0),1));
+    }
+    @Test
+    public void getKeysStatistics() {
+        assertSizeEq(10,callLangsBeanGetKeysStatistics());
+    }
+    @Test
+    public void getRowStatistic1() {
+        assertEq("ACCURACY1",elt(callLangsBeanGetRowStatistic(0),0));
+    }
+    @Test
+    public void getRowStatistic2() {
+        assertEq("ACCURACY2",elt(callLangsBeanGetRowStatistic(0),1));
+    }
+    @Test
+    public void getKeysTargets() {
+        assertSizeEq(13,callLangsBeanGetKeysTargets());
+    }
+    @Test
+    public void getRowTarget1() {
+        assertEq("ADJ_ADV1",elt(callLangsBeanGetRowTarget(0),0));
+    }
+    @Test
+    public void getRowTarget2() {
+        assertEq("ADJ_ADV2",elt(callLangsBeanGetRowTarget(0),1));
+    }
+    @Test
+    public void getKeysCategories() {
+        assertSizeEq(1,callLangsBeanGetKeysCategories());
+    }
+    @Test
+    public void getRowCategory1() {
+        assertEq("C_CA1",elt(callLangsBeanGetRowCategory(0),0));
+    }
+    @Test
+    public void getRowCategory2() {
+        assertEq("C_CA2",elt(callLangsBeanGetRowCategory(0),1));
+    }
+    @Test
+    public void getKeysTypes() {
+        assertSizeEq(1,callLangsBeanGetKeysTypes());
+    }
+    @Test
+    public void getRowType1() {
+        assertEq("T_T1",elt(callLangsBeanGetRowType(0),0));
+    }
+    @Test
+    public void getRowType2() {
+        assertEq("T_T2",elt(callLangsBeanGetRowType(0),1));
+    }
+    @Test
+    public void getKeysAbilities() {
+        assertSizeEq(1,callLangsBeanGetKeysAbilities());
+    }
+    @Test
+    public void getRowAbility1() {
+        assertEq("A_ABILITY1",elt(callLangsBeanGetRowAbility(0),0));
+    }
+    @Test
+    public void getRowAbility2() {
+        assertEq("A_ABILITY2",elt(callLangsBeanGetRowAbility(0),1));
+    }
+    @Test
+    public void getKeysClItems() {
+        assertSizeEq(1,callLangsBeanGetKeysDesc());
+    }
+    @Test
+    public void getRowClItem1() {
+        assertEq("I_ITEM_1",elt(callLangsBeanGetRowDesc(0),0));
+    }
+    @Test
+    public void getRowClItem2() {
+        assertEq("I_ITEM_2",elt(callLangsBeanGetRowDesc(0),1));
+    }
+    @Test
+    public void getKeysItems() {
+        assertSizeEq(1,callLangsBeanGetKeysItems());
+    }
+    @Test
+    public void getRowItem1() {
+        assertEq("I_ITEM1",elt(callLangsBeanGetRowItem(0),0));
+    }
+    @Test
+    public void getRowItem2() {
+        assertEq("I_ITEM2",elt(callLangsBeanGetRowItem(0),1));
+    }
+    @Test
+    public void getKeysMoves() {
+        assertSizeEq(1,callLangsBeanGetKeysMoves());
+    }
+    @Test
+    public void getRowMove1() {
+        assertEq("M_DAM1",elt(callLangsBeanGetRowMove(0),0));
+    }
+    @Test
+    public void getRowMove2() {
+        assertEq("M_DAM2",elt(callLangsBeanGetRowMove(0),1));
+    }
+    @Test
+    public void getKeysPokemon() {
+        assertSizeEq(1,callLangsBeanGetKeysPokemon());
+    }
+    @Test
+    public void getRowPokemon1() {
+        assertEq("P_POKEMON1",elt(callLangsBeanGetRowPokemon(0),0));
+    }
+    @Test
+    public void getRowPokemon2() {
+        assertEq("P_POKEMON2",elt(callLangsBeanGetRowPokemon(0),1));
+    }
+    @Test
+    public void getKeysStatus() {
+        assertSizeEq(1,callLangsBeanGetKeysStatus());
+    }
+    @Test
+    public void getRowStatus1() {
+        assertEq("S_STA_SIM1",elt(callLangsBeanGetRowStatus(0),0));
+    }
+    @Test
+    public void getRowStatus2() {
+        assertEq("S_STA_SIM2",elt(callLangsBeanGetRowStatus(0),1));
+    }
+    @Test
+    public void getKeysFct() {
+        assertSizeEq(1,callLangsBeanGetKeysMath());
+    }
+    @Test
+    public void getRowFct1() {
+        assertEq("lg1",elt(callLangsBeanGetRowMath(0),0));
+    }
+    @Test
+    public void getRowFct2() {
+        assertEq("lg2",elt(callLangsBeanGetRowMath(0),1));
+    }
+}
