3f3632706c9d2d87cbd292725a3b5a1f35603b7c
Cardman <f.desrochettes@gmail.com>
2022-11-18 17:16:25 +0100

    add tests

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/map/InitDbLegPk.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/map/InitDbLegPk.java
@@ -1,61 +1,92 @@
 package aiki.beans.map;
 
-import aiki.beans.BeanPokemonCommonTs;
+import aiki.beans.*;
 import aiki.beans.map.elements.*;
 import code.expressionlanguage.structs.Struct;
+import code.util.*;
 
 public abstract class InitDbLegPk extends InitDbLevelMap{
 
-    public static Struct callLegendaryPokemonBeanClickAbility(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanClickAbility(),_str,_args);
+    public static String callLegendaryPokemonBeanClickAbilityId(int _pk) {
+        Struct bean_ = displayPk(_pk);
+        BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanClickAbility(),bean_);
+        return getValAbilityId(bean_);
     }
 
-    public static Struct callLegendaryPokemonBeanClickItem(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanClickItem(),_str,_args);
+    public static String callLegendaryPokemonBeanClickItem(int _pk) {
+        return callLegendaryPokemonBeanClickItem(displayPk(_pk));
     }
 
-    public static Struct callLegendaryPokemonBeanClickMove(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanClickMove(),_str,_args);
+    public static String callLegendaryPokemonBeanClickItem(Struct _str) {
+        return navigateData(new LegendaryPokemonBeanClickItem(),_str);
     }
 
-    public static Struct callLegendaryPokemonBeanClickName(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanClickName(),_str,_args);
+    public static String callLegendaryPokemonBeanClickItemId(int _pk) {
+        Struct bean_ = displayPk(_pk);
+        callLegendaryPokemonBeanClickItem(bean_);
+        return getValItemId(bean_);
     }
 
-    public static Struct callLegendaryPokemonBeanGetAbility(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetAbility(),_str,_args);
+    public static String callLegendaryPokemonBeanClickMoveId(int _pk, int _move) {
+        Struct bean_ = displayPk(_pk);
+        BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanClickMove(),bean_,_move);
+        return getValMoveId(bean_);
     }
 
-    public static Struct callLegendaryPokemonBeanGetGender(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetGender(),_str,_args);
+    public static String callLegendaryPokemonBeanClickNameId(int _pk) {
+        Struct bean_ = displayPk(_pk);
+        BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanClickName(),bean_);
+        return getValPkId(bean_);
     }
 
-    public static Struct callLegendaryPokemonBeanGetImage(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetImage(),_str,_args);
+    public static Struct callLegendaryPokemonBeanGetAbility(int _pk) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetAbility(),displayPk(_pk));
     }
 
-    public static Struct callLegendaryPokemonBeanGetItem(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetItem(),_str,_args);
+    public static Struct callLegendaryPokemonBeanGetGender(int _pk) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetGender(),displayPk(_pk));
     }
 
-    public static Struct callLegendaryPokemonBeanGetLevel(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetLevel(),_str,_args);
+    public static Struct callLegendaryPokemonBeanGetImage(int _pk) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetImage(),displayPk(_pk));
     }
 
-    public static Struct callLegendaryPokemonBeanGetMove(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetMove(),_str,_args);
+    public static Struct callLegendaryPokemonBeanGetItem(int _pk) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetItem(),displayPk(_pk));
     }
 
-    public static Struct callLegendaryPokemonBeanGetMovesAtLevel(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetMovesAtLevel(),_str,_args);
+    public static Struct callLegendaryPokemonBeanGetLevel(int _pk) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetLevel(),displayPk(_pk));
     }
 
-    public static Struct callLegendaryPokemonBeanGetName(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetName(),_str,_args);
+    public static Struct callLegendaryPokemonBeanGetMove(int _pk, int _move) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetMove(),displayPk(_pk),_move);
     }
 
-    public static Struct callLegendaryPokemonBeanPokemonGet(Struct _str, long... _args) {
-        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanPokemonGet(),_str,_args);
+    public static Struct callLegendaryPokemonBeanGetMovesAtLevel(int _pk) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetMovesAtLevel(),displayPk(_pk));
     }
 
+    public static Struct callLegendaryPokemonBeanGetName(int _pk) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanGetName(),displayPk(_pk));
+    }
+
+    public static Struct callLegendaryPokemonBeanPokemonGet(int _pk) {
+        return BeanPokemonCommonTs.callLongs(new LegendaryPokemonBeanPokemonGet(),displayPk(_pk));
+    }
+    public static Struct displayPk(int _pk){
+        PkData pk_ = pkDataByFacade(db());
+        StringMap<Struct> all_ = beanToMap(pk_);
+        return transitPk(mapInts().get(_pk),pk_,all_);
+    }
+
+    private static Struct transitPk(int _tile,PkData _pk, StringMap<Struct> _all) {
+        Struct bean_ = transitLevel(3,0, _pk, _all);
+        Struct area_ = _all.getVal(AikiBeansMapStd.BEAN_LEG_PK);
+        transit(_pk,new MapLevelBeanClickTileOnMap(),bean_,area_,_tile);
+        return area_;
+    }
+    private static Ints mapInts() {
+        return Ints.newList(18,12);
+    }
 }
---
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/map/elements/LegendaryPokemonBeanTest.java
@@ -0,0 +1,135 @@
+package aiki.beans.map.elements;
+
+import aiki.beans.items.AikiBeansItemsStd;
+import aiki.beans.map.InitDbLegPk;
+import org.junit.Test;
+
+public final class LegendaryPokemonBeanTest extends InitDbLegPk {
+    @Test
+    public void getImage1() {
+        assertEq("AAABAAAS",callLegendaryPokemonBeanGetImage(0));
+    }
+    @Test
+    public void getImage2() {
+        assertEq("AAABAAAT",callLegendaryPokemonBeanGetImage(1));
+    }
+    @Test
+    public void getName1() {
+        assertEq(P_POK_18_TR,callLegendaryPokemonBeanGetName(0));
+    }
+    @Test
+    public void getName2() {
+        assertEq(P_POK_19_TR,callLegendaryPokemonBeanGetName(1));
+    }
+    @Test
+    public void getWpLevel1() {
+        assertEq(4,callWildPkGetLevel(callLegendaryPokemonBeanPokemonGet(0)));
+    }
+    @Test
+    public void getWpLevel2() {
+        assertEq(4,callWildPkGetLevel(callLegendaryPokemonBeanPokemonGet(1)));
+    }
+    @Test
+    public void getLevel1() {
+        assertEq(4,callLegendaryPokemonBeanGetLevel(0));
+    }
+    @Test
+    public void getLevel2() {
+        assertEq(4,callLegendaryPokemonBeanGetLevel(1));
+    }
+    @Test
+    public void getAbility1() {
+        assertEq(A_ABILITY2_TR,callLegendaryPokemonBeanGetAbility(0));
+    }
+    @Test
+    public void getAbility2() {
+        assertEq(A_ABILITY_TR,callLegendaryPokemonBeanGetAbility(1));
+    }
+    @Test
+    public void getItem1() {
+        assertEq(I_REPEL_TR,callLegendaryPokemonBeanGetItem(0));
+    }
+    @Test
+    public void getItem2() {
+        assertEq(I_SELLING_TR,callLegendaryPokemonBeanGetItem(1));
+    }
+    @Test
+    public void getMovesAtLevel1() {
+        assertSizeEq(2,callLegendaryPokemonBeanGetMovesAtLevel(0));
+    }
+    @Test
+    public void getMovesAtLevel2() {
+        assertSizeEq(2,callLegendaryPokemonBeanGetMovesAtLevel(1));
+    }
+    @Test
+    public void getMove1() {
+        assertEq(M_POK_00_TR,callLegendaryPokemonBeanGetMove(0,0));
+    }
+    @Test
+    public void getMove2() {
+        assertEq(M_POK_01_TR,callLegendaryPokemonBeanGetMove(0,1));
+    }
+    @Test
+    public void getMove3() {
+        assertEq(M_POK_02_TR,callLegendaryPokemonBeanGetMove(1,0));
+    }
+    @Test
+    public void getMove4() {
+        assertEq(M_POK_03_TR,callLegendaryPokemonBeanGetMove(1,1));
+    }
+    @Test
+    public void getGender1() {
+        assertEq(NO_G,callLegendaryPokemonBeanGetGender(0));
+    }
+    @Test
+    public void getGender2() {
+        assertEq(NO_G,callLegendaryPokemonBeanGetGender(1));
+    }
+    @Test
+    public void clickName1() {
+        assertEq(P_POK_18,callLegendaryPokemonBeanClickNameId(0));
+    }
+    @Test
+    public void clickName2() {
+        assertEq(P_POK_19,callLegendaryPokemonBeanClickNameId(1));
+    }
+    @Test
+    public void clickMove1() {
+        assertEq(M_POK_00,callLegendaryPokemonBeanClickMoveId(0,0));
+    }
+    @Test
+    public void clickMove2() {
+        assertEq(M_POK_01,callLegendaryPokemonBeanClickMoveId(0,1));
+    }
+    @Test
+    public void clickMove3() {
+        assertEq(M_POK_02,callLegendaryPokemonBeanClickMoveId(1,0));
+    }
+    @Test
+    public void clickMove4() {
+        assertEq(M_POK_03,callLegendaryPokemonBeanClickMoveId(1,1));
+    }@Test
+    public void clickAbility1() {
+        assertEq(A_ABILITY2,callLegendaryPokemonBeanClickAbilityId(0));
+    }
+    @Test
+    public void clickAbility2() {
+        assertEq(A_ABILITY,callLegendaryPokemonBeanClickAbilityId(1));
+    }
+    @Test
+    public void clickItem1() {
+        assertEq(I_REPEL,callLegendaryPokemonBeanClickItemId(0));
+    }
+    @Test
+    public void clickItem2() {
+        assertEq(I_SELLING,callLegendaryPokemonBeanClickItemId(1));
+    }
+    @Test
+    public void clickItem3() {
+        assertEq(AikiBeansItemsStd.WEB_HTML_ITEMS_REPEL_HTML,callLegendaryPokemonBeanClickItem(0));
+    }
+    @Test
+    public void clickItem4() {
+        assertEq(AikiBeansItemsStd.WEB_HTML_ITEMS_SELLINGITEM_HTML,callLegendaryPokemonBeanClickItem(1));
+    }
+}
