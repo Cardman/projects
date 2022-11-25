758b98a5866a9502eaf1e4ce048b7bd9e60fde44
Cardman <f.desrochettes@gmail.com>
2022-11-25 18:54:21 +0100

    add tests

---
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/help/FightHelpBeanMovesEfficiencyTest.java
@@ -0,0 +1,27 @@
+package aiki.beans.help;
+
+import aiki.comparators.DictionaryComparator;
+import aiki.facade.FacadeGame;
+import aiki.fight.util.TypesDuo;
+import code.maths.Rate;
+import code.util.StringMap;
+import org.junit.Test;
+
+public final class FightHelpBeanMovesEfficiencyTest extends InitDbFightHelp {
+    @Test
+    public void movesTypesDefWeatherInitTest() {
+        DictionaryComparator<TypesDuo, Rate> ls_ = FightHelpBean.efficiencyInit(db().getData(),EN);
+        assertEq(4,ls_.size());
+    }
+    private static FacadeGame db() {
+        FacadeGame f_ = facade();
+        f_.getData().getTranslatedTypes().addEntry(EN,new StringMap<String>());
+        f_.getData().getTranslatedTypes().getVal(EN).addEntry(T_TYPE1,T_TYPE1_TR);
+        f_.getData().getTranslatedTypes().getVal(EN).addEntry(T_TYPE2,T_TYPE2_TR);
+        f_.getData().getTableTypes().addEntry(new TypesDuo(T_TYPE1,T_TYPE1), Rate.newRate("2"));
+        f_.getData().getTableTypes().addEntry(new TypesDuo(T_TYPE2,T_TYPE2), Rate.newRate("3"));
+        f_.getData().getTableTypes().addEntry(new TypesDuo(T_TYPE2,T_TYPE1), Rate.newRate("4"));
+        f_.getData().getTableTypes().addEntry(new TypesDuo(T_TYPE1,T_TYPE2), Rate.newRate("5"));
+        return f_;
+    }
+}
