38641a602d4ddf8378ab229a1a475b4af1e86a10
Cardman <f.desrochettes@gmail.com>
2022-11-25 20:10:31 +0100

    add tests

---
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/help/FightHelpBeanMovesTypeDefMovesTest.java
@@ -0,0 +1,37 @@
+package aiki.beans.help;
+
+import aiki.facade.FacadeGame;
+import aiki.fight.moves.DamagingMoveData;
+import aiki.fight.moves.StatusMoveData;
+import aiki.fight.moves.effects.EffectSwitchMoveTypes;
+import aiki.fight.moves.enums.SwitchType;
+import aiki.instances.Instances;
+import code.util.StringList;
+import code.util.StringMap;
+import code.util.core.StringUtil;
+import org.junit.Test;
+
+public final class FightHelpBeanMovesTypeDefMovesTest extends InitDbFightHelp {
+    @Test
+    public void movesTypesDefWeatherInitTest() {
+        StringList ls_ = FightHelpBean.movesTypeDefMovesInit(db().getData());
+        assertEq(1,ls_.size());
+        assertTrue(StringUtil.contains(ls_,M_DAM));
+    }
+    private static FacadeGame db() {
+        FacadeGame f_ = facade();
+        DamagingMoveData t_ = Instances.newDamagingMoveData();
+        EffectSwitchMoveTypes e_ = Instances.newEffectSwitchMoveTypes();
+        e_.getReplacingTypes().add(T_TYPE1);
+        t_.getEffects().add(e_);
+        f_.getData().completeMembers(M_DAM, t_);
+        StatusMoveData s_ = Instances.newStatusMoveData();
+        s_.getEffects().add(Instances.newEffectStatistic());
+        s_.getEffects().add(Instances.newEffectSwitchMoveTypes());
+        f_.getData().completeMembers(M_STA, s_);
+        f_.getData().getTranslatedMoves().addEntry(EN,new StringMap<String>());
+        f_.getData().getTranslatedMoves().getVal(EN).addEntry(M_DAM,M_DAM_TR);
+        f_.getData().getTranslatedMoves().getVal(EN).addEntry(M_STA,M_STA_TR);
+        return f_;
+    }
+}
