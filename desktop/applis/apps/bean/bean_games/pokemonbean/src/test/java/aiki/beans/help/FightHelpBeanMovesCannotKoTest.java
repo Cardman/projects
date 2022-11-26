5a4eb990e1ddfcc5e5c8462e7f01c01a7195f330
Cardman <f.desrochettes@gmail.com>
2022-11-26 09:12:50 +0100

    add tests

--- desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/help/FightHelpBeanMovesCannotKoTest.java
+++ desktop/applis/apps/bean/bean_games/pokemonbean/src/test/java/aiki/beans/help/FightHelpBeanMovesCannotKoTest.java
@@ -20,10 +20,12 @@ public final class FightHelpBeanMovesCannotKoTest extends InitDbFightHelp {
         DamagingMoveData t_ = Instances.newDamagingMoveData();
         t_.setCannotKo(true);
         f_.getData().completeMembers(M_DAM, t_);
-        f_.getData().completeMembers(M_STA, Instances.newStatusMoveData());
+        f_.getData().completeMembers(M_STA, Instances.newDamagingMoveData());
+        f_.getData().completeMembers(M_DAM_VAR, Instances.newStatusMoveData());
         f_.getData().getTranslatedMoves().addEntry(EN,new StringMap<String>());
         f_.getData().getTranslatedMoves().getVal(EN).addEntry(M_DAM,M_DAM_TR);
         f_.getData().getTranslatedMoves().getVal(EN).addEntry(M_STA,M_STA_TR);
+        f_.getData().getTranslatedMoves().getVal(EN).addEntry(M_DAM_VAR,M_DAM_VAR_TR);
         return f_;
     }
 }
