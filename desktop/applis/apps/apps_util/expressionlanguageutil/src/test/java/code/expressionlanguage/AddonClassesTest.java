42b59a984788cb713b32dad48e54552ec83e685d
Cardman <f.desrochettes@gmail.com>
2022-12-17 11:10:26 +0100

    add tests

---
+++ desktop/applis/apps/apps_util/expressionlanguageutil/src/test/java/code/expressionlanguage/AddonClassesTest.java
@@ -0,0 +1,33 @@
+package code.expressionlanguage;
+
+import code.expressionlanguage.exec.*;
+import code.expressionlanguage.exec.util.*;
+import code.expressionlanguage.guicompos.*;
+import code.expressionlanguage.options.*;
+import code.expressionlanguage.utilcompo.*;
+import code.expressionlanguage.utilcompo.stds.*;
+import code.gui.*;
+import code.maths.montecarlo.*;
+import code.mock.*;
+import org.junit.*;
+
+public final class AddonClassesTest extends EquallableElUtUtil {
+    @Test
+    public void executorServiceStruct() {
+        MockProgramInfos pr_ = newMockProgramInfos(new CustomSeedGene(), new MockFileSet(0, new long[1], new String[]{"/"}));
+        LgNamesGui stds_ = newLgNamesGuiSample(pr_, null);
+        Options opt_ = new Options();
+        assertEq(stds_.getCustAliases().getAliasExecutorService(),new ExecutorServiceStruct(pr_.getThreadFactory()).getClassName(stds_.newContext(opt_, getForwards(stds_, opt_))));
+    }
+    @Test
+    public void futureStruct() {
+        MockProgramInfos pr_ = newMockProgramInfos(new CustomSeedGene(), new MockFileSet(0, new long[1], new String[]{"/"}));
+        LgNamesGui stds_ = newLgNamesGuiSample(pr_, null);
+        Options opt_ = new Options();
+        ExecutorServiceStruct essOne_ = new ExecutorServiceStruct(pr_.getThreadFactory(),2);
+        ArgumentListCall list_ = new ArgumentListCall();
+        MockRunnableStruct s_ = new MockRunnableStruct("");
+        list_.getArgumentWrappers().add(new ArgumentWrapper(s_));
+        assertEq(stds_.getCustAliases().getAliasFuture(),new FctExecutorServiceSubmit0().call(null,null,essOne_, list_,null).getValue().getStruct().getClassName(stds_.newContext(opt_, getForwards(stds_, opt_))));
+    }
+}
