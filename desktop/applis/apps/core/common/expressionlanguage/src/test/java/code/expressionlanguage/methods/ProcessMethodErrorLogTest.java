ddae0a78990ba74a14ac2fc01a1f9317756378ff
Cardman <f.desrochettes@gmail.com>
2023-03-21 15:20:24 +0100

    add test

--- desktop/applis/apps/core/common/expressionlanguage/src/test/java/code/expressionlanguage/methods/ProcessMethodErrorLogTest.java
+++ desktop/applis/apps/core/common/expressionlanguage/src/test/java/code/expressionlanguage/methods/ProcessMethodErrorLogTest.java
@@ -85,6 +85,34 @@ public final class ProcessMethodErrorLogTest extends ProcessMethodCommon{
         assertEq("",arg_);
     }
     @Test
+    public void err5() {
+        StringMap<String> files_ = new StringMap<String>();
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("$public $class pkg.Apply {\n");
+        xml_.append(" $public $static Object test(){\n");
+        xml_.append("  $return Apply2.test();\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        files_.put("pkg/Ex", xml_.toString());
+        xml_ = new StringBuilder();
+        xml_.append("$public $class pkg.Apply2 {\n");
+        xml_.append(" $public $static Object test(){\n");
+        xml_.append("  $return String.valueOf(-1,3,'h',' ','w');\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        files_.put("pkg/Ex2", xml_.toString());
+        ContextEl cont_ = ctxOkRead(files_);
+        CustList<Argument> args_ = new CustList<Argument>();
+        MethodId id_ = getMethodId("test");
+        String arg_ = calculateErrorMess("pkg.Apply", id_, args_, cont_);
+        assertEq("code.expressionlanguage.exceptions.BadIndexException\n" +
+                "-1<0\n"+
+                "pkg/Ex:3,18:76\n" +
+                "pkg.Apply.$static test()\n" +
+                "pkg/Ex2:3,18:77\n" +
+                "pkg.Apply2.$static test()",arg_);
+    }
+    @Test
     public void noErr() {
         StringMap<String> files_ = new StringMap<String>();
         StringBuilder xml_ = new StringBuilder();
