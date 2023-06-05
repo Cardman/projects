d94c99e669b18afa8cb7a2e30d5aa3cc06d80d56
Cardman <f.desrochettes@gmail.com>
2023-06-05 09:32:36 +0200

    add possible future feature about a mini debugger (add tests)

--- desktop/applis/apps/core/common/expressionlanguage/src/test/java/code/expressionlanguage/dbg/ProcessDbgReturnTest.java
+++ desktop/applis/apps/core/common/expressionlanguage/src/test/java/code/expressionlanguage/dbg/ProcessDbgReturnTest.java
@@ -1,8 +1,10 @@
 package code.expressionlanguage.dbg;
 
+import code.expressionlanguage.common.NumParsers;
 import code.expressionlanguage.exec.StackCall;
 import code.expressionlanguage.functionid.MethodId;
 import code.expressionlanguage.options.ResultContext;
+import code.expressionlanguage.structs.Struct;
 import code.util.StringMap;
 import org.junit.Test;
 
@@ -121,5 +123,110 @@ public final class ProcessDbgReturnTest extends ProcessDbgCommon {
         MethodId id_ = getMethodId("exmeth");
         StackCall stack_ = dbgNormal("pkg.Ex", id_, cont_);
         assertEq(0, dbgContinueNormal(stack_, cont_.getContext()).nbPages());
+    }
+    @Test
+    public void test7() {
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("public class pkg.Ex {\n");
+        xml_.append(" public int field=8;\n");
+        xml_.append(" public static int exmeth(){\n");
+        xml_.append("  return new Ex().fct(2,3);\n");
+        xml_.append(" }\n");
+        xml_.append(" public int fct(int t, int u){\n");
+        xml_.append("  Fct<int,int,int> fct=(t,u)->field+t+u+#t+#u;\n");
+        xml_.append("  return fct.call(19,13);\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        StringMap<String> files_ = new StringMap<String>();
+        files_.put("pkg/Ex", xml_.toString());
+        ResultContext cont_ = ctxLgReadOnlyOkQuick("en",files_);
+        cont_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("pkg/Ex",164,cont_);
+        MethodId id_ = getMethodId("exmeth");
+        StackCall stack_ = dbgNormal("pkg.Ex", id_, cont_);
+        assertEq(4, stack_.nbPages());
+        assertEq(164, now(stack_));
+        assertEq(2,valueCache(stack_,cont_,"t",0));
+        assertEq(3,valueCache(stack_,cont_,"u",0));
+        assertEq(19,value(stack_,cont_,"t"));
+        assertEq(13,value(stack_,cont_,"u"));
+    }
+    @Test
+    public void test8() {
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("public class pkg.Ex {\n");
+        xml_.append(" public int field=8;\n");
+        xml_.append(" public static int exmeth(){\n");
+        xml_.append("  return new Ex().fct(2,3);\n");
+        xml_.append(" }\n");
+        xml_.append(" public int fct(int t, int u){\n");
+        xml_.append("  Fct<int,int,int> fct=(t,u)->field+t+u+#t+#u;\n");
+        xml_.append("  return fct.call(19,13);\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        StringMap<String> files_ = new StringMap<String>();
+        files_.put("pkg/Ex", xml_.toString());
+        ResultContext cont_ = ctxLgReadOnlyOkQuick("en",files_);
+        cont_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("pkg/Ex",164,cont_);
+        MethodId id_ = getMethodId("exmeth");
+        StackCall stack_ = dbgNormal("pkg.Ex", id_, cont_);
+        assertEq(0, dbgContinueNormal(stack_, cont_.getContext()).nbPages());
+    }
+    @Test
+    public void test9() {
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("public class pkg.Ex {\n");
+        xml_.append(" public int field=8;\n");
+        xml_.append(" public static int exmeth(){\n");
+        xml_.append("  return new Ex().fct(2,3);\n");
+        xml_.append(" }\n");
+        xml_.append(" public int fct(int t, int u){\n");
+        xml_.append("  Fct<int,int,Fct<int,int,int>> fct=(t,u)->(t,u)->field+t+u+#t+#u+##t+##u;\n");
+        xml_.append("  return fct.call(19,13).call(15,17);\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        StringMap<String> files_ = new StringMap<String>();
+        files_.put("pkg/Ex", xml_.toString());
+        ResultContext cont_ = ctxLgReadOnlyOkQuick("en",files_);
+        cont_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("pkg/Ex",184,cont_);
+        MethodId id_ = getMethodId("exmeth");
+        StackCall stack_ = dbgNormal("pkg.Ex", id_, cont_);
+        assertEq(4, stack_.nbPages());
+        assertEq(184, now(stack_));
+        assertEq(2,valueCache(stack_,cont_,"t",1));
+        assertEq(3,valueCache(stack_,cont_,"u",1));
+        assertEq(19,valueCache(stack_,cont_,"t",0));
+        assertEq(13,valueCache(stack_,cont_,"u",0));
+        assertEq(15,value(stack_,cont_,"t"));
+        assertEq(17,value(stack_,cont_,"u"));
+    }
+    @Test
+    public void test10() {
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("public class pkg.Ex {\n");
+        xml_.append(" public int field=8;\n");
+        xml_.append(" public static int exmeth(){\n");
+        xml_.append("  return new Ex().fct(2,3);\n");
+        xml_.append(" }\n");
+        xml_.append(" public int fct(int t, int u){\n");
+        xml_.append("  Fct<int,int,Fct<int,int,int>> fct=(t,u)->(t,u)->field+t+u+#t+#u+##t+##u;\n");
+        xml_.append("  return fct.call(19,13).call(15,17);\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        StringMap<String> files_ = new StringMap<String>();
+        files_.put("pkg/Ex", xml_.toString());
+        ResultContext cont_ = ctxLgReadOnlyOkQuick("en",files_);
+        cont_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("pkg/Ex",184,cont_);
+        MethodId id_ = getMethodId("exmeth");
+        StackCall stack_ = dbgNormal("pkg.Ex", id_, cont_);
+        assertEq(0, dbgContinueNormal(stack_, cont_.getContext()).nbPages());
+    }
+    private static int value(StackCall _stack, ResultContext _cont, String _v) {
+        Struct s_ = _stack.getLastPage().getContentEx().getRefParams().getVal(_v).getValue(_stack, _cont.getContext());
+        return NumParsers.convertToNumber(s_).intStruct();
+    }
+
+    private static int valueCache(StackCall _stack, ResultContext _cont, String _v, int _d) {
+        Struct s_ = _stack.getLastPage().getContentEx().getCache().getLocalWrapper(_v,_d).getValue(_stack, _cont.getContext());
+        return NumParsers.convertToNumber(s_).intStruct();
     }
 }
