0fba1a2cb8b783f91be40f5aff729954e13a22cd
Cardman <f.desrochettes@gmail.com>
2023-06-19 22:50:36 +0200

    add possible future feature about a mini debugger (add tests about enum elements)

--- desktop/applis/apps/core/common/expressionlanguage/src/test/java/code/expressionlanguage/dbg/ProcessDbgStaticTypeTest.java
+++ desktop/applis/apps/core/common/expressionlanguage/src/test/java/code/expressionlanguage/dbg/ProcessDbgStaticTypeTest.java
@@ -57,6 +57,48 @@ public final class ProcessDbgStaticTypeTest extends ProcessDbgCommon {
         StackCall stackLan_ = dbgNormal("pkg.Ex", id_, cont_,next_);
         assertEq(0, stackLan_.nbPages());
     }
+    @Test
+    public void test3() {
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("public enum pkg.Ex {\n");
+        xml_.append(" ONE(2);\n");
+        xml_.append(" int v;\n");
+        xml_.append(" (int i){\n");
+        xml_.append("  v=i;\n");
+        xml_.append(" }\n");
+        xml_.append(" public static int exmeth(){\n");
+        xml_.append("  return Ex.ONE.v;\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        StringMap<String> files_ = new StringMap<String>();
+        files_.put("pkg/Ex", xml_.toString());
+        ResultContext cont_ = ctxLgReadOnlyOkQuick("en",files_);
+        cont_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("pkg/Ex",26,cont_);
+        StackCall stack_ = tryInitStaticlyTypes(cont_.getContext(), cont_.getForwards().getOptions());
+        assertEq(1, stack_.nbPages());
+    }
+    @Test
+    public void test4() {
+        StringBuilder xml_ = new StringBuilder();
+        xml_.append("public enum pkg.Ex {\n");
+        xml_.append(" ONE(2);\n");
+        xml_.append(" int v;\n");
+        xml_.append(" (int i){\n");
+        xml_.append("  v=i;\n");
+        xml_.append(" }\n");
+        xml_.append(" public static int exmeth(){\n");
+        xml_.append("  return Ex.ONE.v;\n");
+        xml_.append(" }\n");
+        xml_.append("}\n");
+        StringMap<String> files_ = new StringMap<String>();
+        files_.put("pkg/Ex", xml_.toString());
+        ResultContext cont_ = ctxLgReadOnlyOkQuick("en",files_);
+        cont_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("pkg/Ex",26,cont_);
+        StackCall stack_ = tryInitStaticlyTypes(cont_.getContext(), cont_.getForwards().getOptions());
+        MethodId id_ = getMethodId("exmeth");
+        StackCall next_ = dbgNormal("pkg.Ex", id_,cont_, stack_);
+        assertEq(0, next_.nbPages());
+    }
     private ExecFileBlock file(ResultContext _cont) {
         return file(_cont,"pkg/Ex");
     }
