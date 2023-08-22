29df537b195b4df37a753713befdae9380f0e1d3
Cardman <f.desrochettes@gmail.com>
2023-08-22 19:41:54 +0200

    add possible future feature about a mini debugger (add tests)

--- desktop/applis/apps/apps_util/expressionlanguageadv/src/test/java/code/expressionlanguage/adv/DbgSyntaxColoringTest.java
+++ desktop/applis/apps/apps_util/expressionlanguageadv/src/test/java/code/expressionlanguage/adv/DbgSyntaxColoringTest.java
@@ -514,6 +514,34 @@ public final class DbgSyntaxColoringTest extends EquallableElAdvUtil {
         CustList<SegmentReadOnlyPart> l_ = list(res_);
         assertEq(0,l_.size());
     }
+    @Test
+    public void parts45() {
+        StringMap<String> src_ = new StringMap<String>();
+        src_.addEntry("src/file.txt", "public annotation pkg.Ex {int exmeth()1;}");
+        ResultContext res_ = ctxReadOnlyOk(src_);
+        res_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("src/file.txt",38,res_);
+        CustList<SegmentReadOnlyPart> l_ = list(res_);
+        assertEq(1,l_.size());
+        assertEq(38,l_.get(0).getBegin());
+        assertEq(39,l_.get(0).getEnd());
+        assertSame(SyntaxRefEnum.INSTRUCTION,l_.get(0).getKind());
+    }
+    @Test
+    public void parts46() {
+        StringMap<String> src_ = new StringMap<String>();
+        src_.addEntry("src/file.txt", "public annotation pkg.Ex {int exmeth()1;}");
+        ResultContext res_ = ctxReadOnlyOk(src_);
+        res_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("src/file.txt",26,res_);
+        res_.getContext().getClasses().getDebugMapping().getBreakPointsBlock().toggleBreakPoint("src/file.txt",38,res_);
+        CustList<SegmentReadOnlyPart> l_ = list(res_);
+        assertEq(2,l_.size());
+        assertEq(38,l_.get(0).getBegin());
+        assertEq(39,l_.get(0).getEnd());
+        assertSame(SyntaxRefEnum.INSTRUCTION,l_.get(0).getKind());
+        assertEq(26,l_.get(1).getBegin());
+        assertEq(38,l_.get(1).getEnd());
+        assertSame(SyntaxRefEnum.FIELD,l_.get(1).getKind());
+    }
     private CustList<SegmentReadOnlyPart> list(ResultContext _res) {
         IdMap<FileBlock,CustList<SegmentReadOnlyPart>> s_ = DbgSyntaxColoring.partsBpMpWp(_res);
         return s_.getVal(_res.getPageEl().getPreviousFilesBodies().getVal("src/file.txt"));
