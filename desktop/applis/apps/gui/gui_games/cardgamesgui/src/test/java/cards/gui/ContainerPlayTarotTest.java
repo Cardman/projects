bdd25a6b0955419d89efe368c4a1edb6ca5383ce
Cardman <f.desrochettes@gmail.com>
2024-02-22 15:56:21 +0100

    add tests about displaying carpet

--- desktop/applis/apps/gui/gui_games/cardgamesgui/src/test/java/cards/gui/ContainerPlayTarotTest.java
+++ desktop/applis/apps/gui/gui_games/cardgamesgui/src/test/java/cards/gui/ContainerPlayTarotTest.java
@@ -2895,6 +2895,68 @@ public final class ContainerPlayTarotTest extends EquallableCardsGuiUtil {
         assertTrue(tr3_.containsObj(cst_.getHandfulsRadio().getVal(Handfuls.NO).getPaintableLabel()));
         assertEq(Handfuls.NO,cst_.getChoosenHandful());
     }
+    @Test
+    public void p79() {
+        RulesTarot rules_ = rulesThree();
+        DealTarot deal_ = deal6(0);
+        MockGameTarot mock_ = new MockGameTarot();
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        ContainerSingleTarot cst_ = editTarot(rules_, deal_, mock_);
+        tryAnimate(cst_);
+        tryClickBid(cst_,mock_);
+        tryAnimate(cst_);
+        IdList<AbsCustComponent> tr_ = ((MockCustComponent) cst_.window().getPane()).getTreeAccessible();
+        assertEq(1, tr_.size());
+    }
+    @Test
+    public void p80() {
+        RulesTarot rules_ = rulesThree();
+        DealTarot deal_ = deal6(0);
+        MockGameTarot mock_ = new MockGameTarot();
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        ContainerSingleTarot cst_ = editTarotOtherDisplay(rules_, deal_, mock_);
+        tryAnimate(cst_);
+        tryClickBid(cst_,mock_);
+        tryAnimate(cst_);
+        IdList<AbsCustComponent> tr_ = ((MockCustComponent) cst_.window().getPane()).getTreeAccessible();
+        assertEq(1, tr_.size());
+    }
+    @Test
+    public void p81() {
+        RulesTarot rules_ = rulesFour();
+        DealTarot deal_ = deal5(0);
+        MockGameTarot mock_ = new MockGameTarot();
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        ContainerSingleTarot cst_ = editTarot(rules_, deal_, mock_);
+        tryAnimate(cst_);
+        tryClickBid(cst_,mock_);
+        tryAnimate(cst_);
+        IdList<AbsCustComponent> tr_ = ((MockCustComponent) cst_.window().getPane()).getTreeAccessible();
+        assertEq(1, tr_.size());
+    }
+    @Test
+    public void p82() {
+        RulesTarot rules_ = rulesFour();
+        DealTarot deal_ = deal5(0);
+        MockGameTarot mock_ = new MockGameTarot();
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        nextBid(mock_, BidTarot.FOLD);
+        ContainerSingleTarot cst_ = editTarotOtherDisplay(rules_, deal_, mock_);
+        tryAnimate(cst_);
+        tryClickBid(cst_,mock_);
+        tryAnimate(cst_);
+        IdList<AbsCustComponent> tr_ = ((MockCustComponent) cst_.window().getPane()).getTreeAccessible();
+        assertEq(1, tr_.size());
+    }
     private void clickUniqButton(ContainerSingleTarot _cst) {
         IdList<AbsCustComponent> tr4_ = ((MockCustComponent) _cst.window().getPane()).getTreeAccessible();
         assertEq(1, tr4_.size());
@@ -3102,6 +3164,20 @@ public final class ContainerPlayTarotTest extends EquallableCardsGuiUtil {
         rules_.getCommon().setNbDeals(1);
         rules_.getCommon().setMixedCards(MixCardsChoice.NEVER);
         rules_.setDealing(DealingTarot.DEAL_2_VS_4_WITHOUT_CALL);
+        return rules_;
+    }
+
+    private RulesTarot rulesFour() {
+        RulesTarot rules_ = new RulesTarot((byte) 4);
+        rules_.getCommon().setNbDeals(1);
+        rules_.getCommon().setMixedCards(MixCardsChoice.NEVER);
+        return rules_;
+    }
+
+    private RulesTarot rulesThree() {
+        RulesTarot rules_ = new RulesTarot((byte) 3);
+        rules_.getCommon().setNbDeals(1);
+        rules_.getCommon().setMixedCards(MixCardsChoice.NEVER);
         return rules_;
     }
 
@@ -3412,7 +3488,6 @@ public final class ContainerPlayTarotTest extends EquallableCardsGuiUtil {
         hand_.ajouter(CardTarot.SPADE_KING);
         hand_.ajouter(CardTarot.SPADE_QUEEN);
         hands_.add(hand_);
-        //full_.supprimerCartes(hand_);
         hand_ = new HandTarot();
         hand_.ajouter(CardTarot.SPADE_10);
         hand_.ajouter(CardTarot.HEART_7);
@@ -3427,7 +3502,6 @@ public final class ContainerPlayTarotTest extends EquallableCardsGuiUtil {
         hand_.ajouter(CardTarot.DIAMOND_7);
         hand_.ajouter(CardTarot.DIAMOND_6);
         hands_.add(hand_);
-        //full_.supprimerCartes(hand_);
         hand_ = new HandTarot();
         hand_.ajouter(CardTarot.TRUMP_2);
         hand_.ajouter(CardTarot.TRUMP_8);
@@ -3442,7 +3516,6 @@ public final class ContainerPlayTarotTest extends EquallableCardsGuiUtil {
         hand_.ajouter(CardTarot.DIAMOND_KNIGHT);
         hand_.ajouter(CardTarot.DIAMOND_1);
         hands_.add(hand_);
-        //full_.supprimerCartes(hand_);
         hand_ = new HandTarot();
         hand_.ajouter(CardTarot.TRUMP_5);
         hand_.ajouter(CardTarot.TRUMP_9);
@@ -3457,7 +3530,6 @@ public final class ContainerPlayTarotTest extends EquallableCardsGuiUtil {
         hand_.ajouter(CardTarot.DIAMOND_9);
         hand_.ajouter(CardTarot.DIAMOND_3);
         hands_.add(hand_);
-        //full_.supprimerCartes(hand_);
         hand_ = new HandTarot();
         hand_.ajouter(CardTarot.TRUMP_11);
         hand_.ajouter(CardTarot.TRUMP_18);
@@ -3497,4 +3569,189 @@ public final class ContainerPlayTarotTest extends EquallableCardsGuiUtil {
         return new DealTarot(hands_, (byte) _dealer);
     }
 
+    private static DealTarot deal5(int _dealer) {
+        CustList<HandTarot> hands_ = new CustList<HandTarot>();
+        HandTarot hand_;
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.TRUMP_1);
+        hand_.ajouter(CardTarot.TRUMP_4);
+        hand_.ajouter(CardTarot.TRUMP_10);
+        hand_.ajouter(CardTarot.TRUMP_14);
+        hand_.ajouter(CardTarot.TRUMP_17);
+        hand_.ajouter(CardTarot.TRUMP_21);
+        hand_.ajouter(CardTarot.TRUMP_3);
+        hand_.ajouter(CardTarot.TRUMP_6);
+        hand_.ajouter(CardTarot.TRUMP_13);
+        hand_.ajouter(CardTarot.HEART_KING);
+        hand_.ajouter(CardTarot.SPADE_KING);
+        hand_.ajouter(CardTarot.SPADE_QUEEN);
+        hand_.ajouter(CardTarot.SPADE_10);
+        hand_.ajouter(CardTarot.HEART_7);
+        hand_.ajouter(CardTarot.DIAMOND_2);
+        hand_.ajouter(CardTarot.HEART_1);
+        hand_.ajouter(CardTarot.HEART_8);
+        hand_.ajouter(CardTarot.HEART_4);
+        hands_.add(hand_);
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.SPADE_KNIGHT);
+        hand_.ajouter(CardTarot.SPADE_JACK);
+        hand_.ajouter(CardTarot.CLUB_10);
+        hand_.ajouter(CardTarot.CLUB_4);
+        hand_.ajouter(CardTarot.DIAMOND_7);
+        hand_.ajouter(CardTarot.DIAMOND_6);
+        hand_.ajouter(CardTarot.TRUMP_2);
+        hand_.ajouter(CardTarot.TRUMP_8);
+        hand_.ajouter(CardTarot.TRUMP_12);
+        hand_.ajouter(CardTarot.HEART_3);
+        hand_.ajouter(CardTarot.HEART_2);
+        hand_.ajouter(CardTarot.SPADE_5);
+        hand_.ajouter(CardTarot.SPADE_2);
+        hand_.ajouter(CardTarot.SPADE_1);
+        hand_.ajouter(CardTarot.CLUB_9);
+        hand_.ajouter(CardTarot.CLUB_KNIGHT);
+        hand_.ajouter(CardTarot.DIAMOND_KNIGHT);
+        hand_.ajouter(CardTarot.DIAMOND_1);
+        hands_.add(hand_);
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.TRUMP_5);
+        hand_.ajouter(CardTarot.TRUMP_9);
+        hand_.ajouter(CardTarot.TRUMP_15);
+        hand_.ajouter(CardTarot.HEART_KNIGHT);
+        hand_.ajouter(CardTarot.HEART_10);
+        hand_.ajouter(CardTarot.SPADE_8);
+        hand_.ajouter(CardTarot.SPADE_3);
+        hand_.ajouter(CardTarot.CLUB_JACK);
+        hand_.ajouter(CardTarot.CLUB_8);
+        hand_.ajouter(CardTarot.DIAMOND_5);
+        hand_.ajouter(CardTarot.DIAMOND_9);
+        hand_.ajouter(CardTarot.DIAMOND_3);
+        hand_.ajouter(CardTarot.TRUMP_11);
+        hand_.ajouter(CardTarot.TRUMP_18);
+        hand_.ajouter(CardTarot.HEART_6);
+        hand_.ajouter(CardTarot.HEART_5);
+        hand_.ajouter(CardTarot.SPADE_9);
+        hand_.ajouter(CardTarot.SPADE_7);
+        hands_.add(hand_);
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.SPADE_6);
+        hand_.ajouter(CardTarot.DIAMOND_QUEEN);
+        hand_.ajouter(CardTarot.DIAMOND_10);
+        hand_.ajouter(CardTarot.CLUB_KING);
+        hand_.ajouter(CardTarot.CLUB_2);
+        hand_.ajouter(CardTarot.CLUB_1);
+        hand_.ajouter(CardTarot.TRUMP_20);
+        hand_.ajouter(CardTarot.TRUMP_16);
+        hand_.ajouter(CardTarot.HEART_QUEEN);
+        hand_.ajouter(CardTarot.DIAMOND_KING);
+        hand_.ajouter(CardTarot.CLUB_3);
+        hand_.ajouter(CardTarot.CLUB_QUEEN);
+        hand_.ajouter(CardTarot.DIAMOND_8);
+        hand_.ajouter(CardTarot.DIAMOND_4);
+        hand_.ajouter(CardTarot.EXCUSE);
+        hand_.ajouter(CardTarot.HEART_9);
+        hand_.ajouter(CardTarot.CLUB_7);
+        hand_.ajouter(CardTarot.CLUB_5);
+        hands_.add(hand_);
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.TRUMP_19);
+        hand_.ajouter(CardTarot.TRUMP_7);
+        hand_.ajouter(CardTarot.HEART_JACK);
+        hand_.ajouter(CardTarot.SPADE_4);
+        hand_.ajouter(CardTarot.CLUB_6);
+        hand_.ajouter(CardTarot.DIAMOND_JACK);
+        hands_.add(hand_);
+        return new DealTarot(hands_, (byte) _dealer);
+    }
+
+    private static DealTarot deal6(int _dealer) {
+        CustList<HandTarot> hands_ = new CustList<HandTarot>();
+        HandTarot hand_;
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.TRUMP_1);
+        hand_.ajouter(CardTarot.TRUMP_4);
+        hand_.ajouter(CardTarot.TRUMP_10);
+        hand_.ajouter(CardTarot.TRUMP_14);
+        hand_.ajouter(CardTarot.TRUMP_17);
+        hand_.ajouter(CardTarot.TRUMP_21);
+        hand_.ajouter(CardTarot.TRUMP_3);
+        hand_.ajouter(CardTarot.TRUMP_6);
+        hand_.ajouter(CardTarot.TRUMP_13);
+        hand_.ajouter(CardTarot.HEART_KING);
+        hand_.ajouter(CardTarot.SPADE_KING);
+        hand_.ajouter(CardTarot.SPADE_QUEEN);
+        hand_.ajouter(CardTarot.SPADE_10);
+        hand_.ajouter(CardTarot.HEART_7);
+        hand_.ajouter(CardTarot.DIAMOND_2);
+        hand_.ajouter(CardTarot.HEART_1);
+        hand_.ajouter(CardTarot.HEART_8);
+        hand_.ajouter(CardTarot.HEART_4);
+        hand_.ajouter(CardTarot.SPADE_KNIGHT);
+        hand_.ajouter(CardTarot.SPADE_JACK);
+        hand_.ajouter(CardTarot.CLUB_10);
+        hand_.ajouter(CardTarot.CLUB_4);
+        hand_.ajouter(CardTarot.DIAMOND_7);
+        hand_.ajouter(CardTarot.DIAMOND_6);
+        hands_.add(hand_);
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.TRUMP_2);
+        hand_.ajouter(CardTarot.TRUMP_8);
+        hand_.ajouter(CardTarot.TRUMP_12);
+        hand_.ajouter(CardTarot.HEART_3);
+        hand_.ajouter(CardTarot.HEART_2);
+        hand_.ajouter(CardTarot.SPADE_5);
+        hand_.ajouter(CardTarot.SPADE_2);
+        hand_.ajouter(CardTarot.SPADE_1);
+        hand_.ajouter(CardTarot.CLUB_9);
+        hand_.ajouter(CardTarot.CLUB_KNIGHT);
+        hand_.ajouter(CardTarot.DIAMOND_KNIGHT);
+        hand_.ajouter(CardTarot.DIAMOND_1);
+        hand_.ajouter(CardTarot.TRUMP_5);
+        hand_.ajouter(CardTarot.TRUMP_9);
+        hand_.ajouter(CardTarot.TRUMP_15);
+        hand_.ajouter(CardTarot.HEART_KNIGHT);
+        hand_.ajouter(CardTarot.HEART_10);
+        hand_.ajouter(CardTarot.SPADE_8);
+        hand_.ajouter(CardTarot.SPADE_3);
+        hand_.ajouter(CardTarot.CLUB_JACK);
+        hand_.ajouter(CardTarot.CLUB_8);
+        hand_.ajouter(CardTarot.DIAMOND_5);
+        hand_.ajouter(CardTarot.DIAMOND_9);
+        hand_.ajouter(CardTarot.DIAMOND_3);
+        hands_.add(hand_);
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.TRUMP_11);
+        hand_.ajouter(CardTarot.TRUMP_18);
+        hand_.ajouter(CardTarot.HEART_6);
+        hand_.ajouter(CardTarot.HEART_5);
+        hand_.ajouter(CardTarot.SPADE_9);
+        hand_.ajouter(CardTarot.SPADE_7);
+        hand_.ajouter(CardTarot.SPADE_6);
+        hand_.ajouter(CardTarot.DIAMOND_QUEEN);
+        hand_.ajouter(CardTarot.DIAMOND_10);
+        hand_.ajouter(CardTarot.CLUB_KING);
+        hand_.ajouter(CardTarot.CLUB_2);
+        hand_.ajouter(CardTarot.CLUB_1);
+        hand_.ajouter(CardTarot.TRUMP_20);
+        hand_.ajouter(CardTarot.TRUMP_16);
+        hand_.ajouter(CardTarot.HEART_QUEEN);
+        hand_.ajouter(CardTarot.DIAMOND_KING);
+        hand_.ajouter(CardTarot.CLUB_3);
+        hand_.ajouter(CardTarot.CLUB_QUEEN);
+        hand_.ajouter(CardTarot.DIAMOND_8);
+        hand_.ajouter(CardTarot.DIAMOND_4);
+        hand_.ajouter(CardTarot.EXCUSE);
+        hand_.ajouter(CardTarot.HEART_9);
+        hand_.ajouter(CardTarot.CLUB_7);
+        hand_.ajouter(CardTarot.CLUB_5);
+        hands_.add(hand_);
+        hand_ = new HandTarot();
+        hand_.ajouter(CardTarot.TRUMP_19);
+        hand_.ajouter(CardTarot.TRUMP_7);
+        hand_.ajouter(CardTarot.HEART_JACK);
+        hand_.ajouter(CardTarot.SPADE_4);
+        hand_.ajouter(CardTarot.CLUB_6);
+        hand_.ajouter(CardTarot.DIAMOND_JACK);
+        hands_.add(hand_);
+        return new DealTarot(hands_, (byte) _dealer);
+    }
 }
