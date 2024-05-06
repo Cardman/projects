d4e1cdae725bbb5326ed95c2c5401c561343975c
Cardman <f.desrochettes@gmail.com>
2024-05-06 16:30:20 +0200

    try removing useless field

--- desktop/applis/apps/gui/gui_games/pokemongui/src/main/java/code/gui/document/ThreadRefresh.java
+++ desktop/applis/apps/gui/gui_games/pokemongui/src/main/java/code/gui/document/ThreadRefresh.java
@@ -1,20 +1,19 @@
 package code.gui.document;
 
-import code.bean.nat.BeanNatCommonLgNames;
 import code.bean.nat.FixCharacterCaseConverter;
-import code.sml.RendKeyWordsGroup;
 import code.formathtml.render.MetaDocument;
 import code.sml.Document;
+import code.sml.RendKeyWordsGroup;
 
 public final class ThreadRefresh implements Runnable {
 
     private final RenderedPage page;
 
-    private final BeanNatCommonLgNames stds;
+//    private final BeanNatCommonLgNames stds;
 
-    ThreadRefresh(RenderedPage _page, BeanNatCommonLgNames _lgNames) {
+    ThreadRefresh(RenderedPage _page) {
+//    ThreadRefresh(RenderedPage _page, BeanNatCommonLgNames _lgNames)
         page = _page;
-        stds = _lgNames;
     }
 
     @Override
@@ -24,21 +23,21 @@ public final class ThreadRefresh implements Runnable {
         afterAction();
     }
     private void afterAction() {
-        if (!page.isProcessing()) {
-            return;
-        }
+//        if (!page.isProcessing()) {
+//            return;
+//        }
         Document doc_ = page.getNavCore().getDocument();
 //        Document doc_ = page.getNavigation().getDocument();
-        if (doc_ == null) {
-            finish();
-            return;
-        }
+//        if (doc_ == null) {
+//            finish();
+//            return;
+//        }
         MetaDocument metadoc_ = MetaDocument.newInstance(doc_,new RendKeyWordsGroup(),"ABCDEF",new FixCharacterCaseConverter());
 //        MetaDocument metadoc_ = MetaDocument.newInstance(doc_,page.getNavigation().getSession().getRendKeyWords());
         page.getGene().getCompoFactory().invokeNow(new WindowPage(metadoc_, page.getScroll(), page));
     }
-
-    private void finish() {
-        page.finish();
-    }
+//
+//    private void finish() {
+//        page.finish();
+//    }
 }
