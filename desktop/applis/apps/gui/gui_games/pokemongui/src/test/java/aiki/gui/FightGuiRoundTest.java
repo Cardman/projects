2d6139db0b7aac2d715c583c59b67aaecf652c3c
Cardman <f.desrochettes@gmail.com>
2024-05-07 17:39:41 +0200

    add tests about fight animation

--- desktop/applis/apps/gui/gui_games/pokemongui/src/test/java/aiki/gui/FightGuiRoundTest.java
+++ desktop/applis/apps/gui/gui_games/pokemongui/src/test/java/aiki/gui/FightGuiRoundTest.java
@@ -3,6 +3,7 @@ package aiki.gui;
 import aiki.db.*;
 import aiki.fight.enums.Statistic;
 import aiki.fight.items.Ball;
+import aiki.fight.items.HealingPp;
 import aiki.fight.moves.DamagingMoveData;
 import aiki.fight.moves.MoveData;
 import aiki.fight.moves.StatusMoveData;
@@ -13,6 +14,8 @@ import aiki.game.UsesOfMove;
 import aiki.game.fight.Fight;
 import aiki.game.fight.actions.ActionSwitch;
 import aiki.game.fight.enums.ActionType;
+import aiki.gui.components.PaginatorHealingItem;
+import aiki.gui.dialogs.SelectHealingItem;
 import aiki.instances.*;
 import aiki.map.DataMap;
 import aiki.map.characters.*;
@@ -623,6 +626,36 @@ public final class FightGuiRoundTest extends InitDbGuiAiki {
         assertEq(1, tree_.size());
         assertTrue(tree_.containsObj(window_.getBattle().getBattle().getValidateActions()));
     }
+
+    @Test
+    public void eff24() {
+        WindowAiki window_ = newFight();
+        coreDataBaseTrainerHealBack(window_);
+        tryPress(window_.getScenePanel().getScene(), GuiConstants.VK_RIGHT);
+        tryAn((MockThreadFactory) window_.getFrames().getThreadFactory());
+        tryClick(window_.getScenePanel().getButtonInteract());
+        tryAn((MockThreadFactory) window_.getFrames().getThreadFactory());
+        window_.getBattle().getBattle().getFighterBackPanel().getListe().select(1);
+        window_.getBattle().getBattle().getFighterBackPanel().getListe().events();
+        tryClick(window_.getBattle().getBattle().getActionsLabels().get(window_.getFacade().getFight().getTemp().getPossibleActionsCurFighter().indexOfObj(ActionType.HEALING)));
+        tryClick(window_.getBattle().getBattle().getChooseHealItem());
+        SelectHealingItem sel_ = window_.getSelectHealingItem();
+        PaginatorHealingItem pag_ = sel_.getPaginatorHealingItem();
+        pag_.getName().setText("graisse");
+        tryClick(pag_.getSearchButton());
+        tryClick(pag_.getResultsLabels().get(0));
+        tryClick(sel_.getOkButton());
+        ActionSwitch as_ = new ActionSwitch();
+        as_.setSubstitute((byte) 1);
+        window_.getFacade().getFight().getFighter(Fight.toFoeFighter((byte) 0)).setAction(as_);
+        tryClick(window_.getBattle().getBattle().getValidateActions());
+        tryAn((MockThreadFactory) window_.getFrames().getThreadFactory());
+        IdList<AbsCustComponent> tree_ = ((MockCustComponent) window_.getBattle().getBattle().getPane()).getTreeAccessible();
+        assertEq(3, tree_.size());
+        assertTrue(tree_.containsObj(window_.getBattle().getBattle().getFighterFrontPanel().getListe().getGlobal()));
+        assertTrue(tree_.containsObj(window_.getBattle().getBattle().getFighterBackPanel().getListe().getGlobal()));
+        assertTrue(tree_.containsObj(window_.getBattle().getBattle().getValidateActions()));
+    }
     @Test
     public void effNo1() {
         WindowAiki window_ = newFight();
@@ -960,6 +993,17 @@ public final class FightGuiRoundTest extends InitDbGuiAiki {
         game_.getPlayer().getTeam().add(pk(_window));
         game_.getPlayer().getTeam().add(pk(_window));
     }
+
+    private static void coreDataBaseTrainerHealBack(WindowAiki _window) {
+        loadRom(_window, coreDataBaseTrainerHealBack());
+        _window.getFacade().healTr();
+        Game game_ = build(_window.getFacade());
+        loadGame(_window, game_);
+        game_.getPlayer().getTeam().add(pk(_window));
+        game_.getPlayer().getTeam().add(pk(_window));
+        game_.getPlayer().getItem(HUILE);
+        ((PokemonPlayer)game_.getPlayer().getTeam().last()).getMoves().getValue(0).setCurrent((short) 0);
+    }
     public static DataBase coreDataBaseTrainer2() {
         DataBase data_ = init();
         initDefaultConsts(POKE_BALL,"1","1","1","1","1", ECLAIR_2, PIKACHU, data_);
@@ -978,6 +1022,55 @@ public final class FightGuiRoundTest extends InitDbGuiAiki {
         DataBase mv_ = withMv(withMv(withMv(ab_, ECLAIR_4, trsMv_, "biz 4"), ECLAIR_2, trsMv_, "biz 2"), ECLAIR, trsMv_, "biz");
         DataBase res_ = withPk(withPk(mv_, RAICHU, trsPk_, RAICHU_TR), PIKACHU, trsPk_, PIKACHU_TR);
         DataBase ball_ = withIt(res_, POKE_BALL, trsIt_, "ball");
+        initBegin(data_);
+
+        City city_ = withBlocksPkCenter(withBlocks(Instances.newCity()), newGerantPokemon(GeranceType.HOST));
+        data_.getMap().addPlace(city_);
+        Road road_ = withBlocks(Instances.newRoad());
+        TrainerMultiFights tr_ = Instances.newTrainerMultiFights();
+        tr_.setImageMaxiFileName(SNOW);
+        PokemonTeam team_ = Instances.newPokemonTeam();
+        team_.getTeam().add(new PkTrainer(wild(),new StringList(ECLAIR)));
+        team_.getTeam().add(new PkTrainer(wild(RAICHU,1,PARATONNERRE,NULL_REF),new StringList(ECLAIR)));
+        tr_.getTeamsRewards().add(team_);
+        road_.getLevelRoad().getCharacters().addEntry(newPoint(0,1), tr_);
+        data_.getMap().addPlace(road_);
+        data_.addTrainerImage(SNOW, new int[][]{new int[1]});
+
+
+//        initMiniMap(data_);
+        data_.getTm().addEntry((short)2,ECLAIR);
+        data_.getTm().addEntry((short)3,ECLAIR_4);
+        data_.getTmPrice().addEntry((short)2,new LgInt("1"));
+        data_.getTmPrice().addEntry((short)3,new LgInt("2"));
+        city_.getSavedlinks().addEntry(new PlaceInterConnect(newPoint(2,0), Direction.RIGHT),newCoords(1,0,0,0));
+        road_.getSavedlinks().addEntry(new PlaceInterConnect(newPoint(0,0),Direction.LEFT),newCoords(0,0,2,0));
+        compute(data_);
+
+        return ball_;
+    }
+    public static DataBase coreDataBaseTrainerHealBack() {
+        DataBase data_ = init();
+        initDefaultConsts(POKE_BALL,"1","1","1","1","1", ECLAIR_2, PIKACHU, data_);
+        StringMap<String> trsIt_ = new StringMap<String>();
+        StringMap<String> trsDes_ = new StringMap<String>();
+        StringMap<String> trsPk_ = new StringMap<String>();
+        StringMap<String> trsMv_ = new StringMap<String>();
+        StringMap<String> trsAb_ = new StringMap<String>();
+        StringMap<String> trsTypes_ = new StringMap<String>();
+        data_.getTranslatedPokemon().addEntry(LANGUAGE, trsPk_);
+        data_.getTranslatedMoves().addEntry(LANGUAGE, trsMv_);
+        data_.getTranslatedAbilities().addEntry(LANGUAGE, trsAb_);
+        data_.getTranslatedItems().addEntry(LANGUAGE, trsIt_);
+        data_.getTranslatedClassesDescriptions().addEntry(LANGUAGE, trsDes_);
+        data_.getTranslatedTypes().addEntry(LANGUAGE, trsTypes_);
+        trsTypes_.put(ELECTRICK,"elec");
+        DataBase ab_ = withAb(data_, PARATONNERRE, trsAb_, "parra");
+        DataBase mv_ = withMv(withMv(withMv(ab_, ECLAIR_4, trsMv_, "biz 4"), ECLAIR_2, trsMv_, "biz 2"), ECLAIR, trsMv_, "biz");
+        DataBase res_ = withPk(withPk(mv_, RAICHU, trsPk_, RAICHU_TR), PIKACHU, trsPk_, PIKACHU_TR);
+        HealingPp pp_ = Instances.newHealingPp();
+        pp_.setHealingAllMovesPp(true);
+        DataBase ball_ = withIt(withIt(res_, HUILE, trsIt_, "graisse", pp_, trsDes_, "grace"), POKE_BALL, trsIt_, "ball", Instances.newBall(), trsDes_, "chance");
         initBegin(data_);
 
         City city_ = withBlocksPkCenter(withBlocks(Instances.newCity()), newGerantPokemon(GeranceType.HOST));
