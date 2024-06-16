15cf925c512e6c30ab6bbd62411ef96748b9521a
Cardman <f.desrochettes@gmail.com>
2024-06-16 09:11:48 +0200

    prepare optimizing network send/receive

--- desktop/applis/apps/gui/network/src/main/java/aiki/network/NetAikiRetrievedInfos.java
+++ desktop/applis/apps/gui/network/src/main/java/aiki/network/NetAikiRetrievedInfos.java
@@ -1,6 +1,7 @@
 package aiki.network;
 
 import cards.network.common.ClientServerIdInfos;
+import cards.network.threads.Net;
 import code.util.CustList;
 import code.util.core.NumberUtil;
 
@@ -9,7 +10,7 @@ public final class NetAikiRetrievedInfos {
     private final CustList<String> parts;
     public NetAikiRetrievedInfos(String _info){
         int len_ = _info.length();
-        ClientServerIdInfos cs_ = new ClientServerIdInfos(_info,NetAiki.AIKI_SEP_0);
+        ClientServerIdInfos cs_ = new ClientServerIdInfos(_info, Net.SEP_0);
         StringBuilder id_ = cs_.getId();
         int index_ = cs_.getIndex();
 //        while (index_ < _info.length()) {
