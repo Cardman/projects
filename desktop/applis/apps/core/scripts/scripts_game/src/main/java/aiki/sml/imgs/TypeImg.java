3f7cb0eb7a11c45bc2c5980b240f740f34bb3b4f
Cardman <f.desrochettes@gmail.com>
2024-10-13 16:58:36 +0200

    refactor constants

--- desktop/applis/apps/core/scripts/scripts_game/src/main/java/aiki/sml/imgs/TypeImg.java
+++ desktop/applis/apps/core/scripts/scripts_game/src/main/java/aiki/sml/imgs/TypeImg.java
@@ -1,27 +1,28 @@
 package aiki.sml.imgs;
 import code.util.*;
-public final class TypeImg{
+import aiki.sml.init.*;
+public final class TypeImg extends CstIgame{
 private TypeImg(){}
 public static StringMap<String> im(){
 StringMap<String> i = new StringMap<String>(18);
-i.addEntry("ACIER",TypeImg0.i0());
-i.addEntry("COMBAT",TypeImg0.i1());
-i.addEntry("DRAGON",TypeImg0.i2());
-i.addEntry("EAU",TypeImg0.i3());
-i.addEntry("ELECTRIQUE",TypeImg0.i4());
-i.addEntry("FEE",TypeImg0.i5());
-i.addEntry("FEU",TypeImg0.i6());
-i.addEntry("GLACE",TypeImg0.i7());
-i.addEntry("INSECTE",TypeImg0.i8());
-i.addEntry("NORMAL",TypeImg0.i9());
-i.addEntry("PLANTE",TypeImg0.i10());
-i.addEntry("POISON",TypeImg0.i11());
-i.addEntry("PSY",TypeImg0.i12());
-i.addEntry("ROCHE",TypeImg0.i13());
-i.addEntry("SOL",TypeImg0.i14());
-i.addEntry("SPECTRE",TypeImg0.i15());
-i.addEntry("TENEBRE",TypeImg0.i16());
-i.addEntry("VOL",TypeImg0.i17());
+i.addEntry(I_ACIER,TypeImg0.i0());
+i.addEntry(I_COMBAT,TypeImg0.i1());
+i.addEntry(I_DRAGON,TypeImg0.i2());
+i.addEntry(I_EAU,TypeImg0.i3());
+i.addEntry(I_ELECTRIQUE,TypeImg0.i4());
+i.addEntry(I_FEE,TypeImg0.i5());
+i.addEntry(I_FEU,TypeImg0.i6());
+i.addEntry(I_GLACE,TypeImg0.i7());
+i.addEntry(I_INSECTE,TypeImg0.i8());
+i.addEntry(I_NORMAL,TypeImg0.i9());
+i.addEntry(I_PLANTE,TypeImg0.i10());
+i.addEntry(I_POISON,TypeImg0.i11());
+i.addEntry(I_PSY,TypeImg0.i12());
+i.addEntry(I_ROCHE,TypeImg0.i13());
+i.addEntry(I_SOL,TypeImg0.i14());
+i.addEntry(I_SPECTRE,TypeImg0.i15());
+i.addEntry(I_TENEBRE,TypeImg0.i16());
+i.addEntry(I_VOL,TypeImg0.i17());
 return i;
 }
 }
