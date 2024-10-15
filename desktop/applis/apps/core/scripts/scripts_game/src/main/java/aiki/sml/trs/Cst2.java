691a84e488778283b2260f0dfbf2f809ca8aeee9
Cardman <f.desrochettes@gmail.com>
2024-10-15 19:48:59 +0200

    refactor strings in script

--- desktop/applis/apps/core/scripts/scripts_game/src/main/java/aiki/sml/trs/Cst2.java
+++ desktop/applis/apps/core/scripts/scripts_game/src/main/java/aiki/sml/trs/Cst2.java
@@ -1,17 +1,18 @@
 package aiki.sml.trs;
+import aiki.sml.init.*;
 import code.maths.litteral.*;
 import code.util.*;
 import aiki.fight.pokemon.enums.*;
-public final class Cst2{
+public final class Cst2 extends CstIgame {
 private Cst2(){}
-static IdMap<ExpType,String> ex(){
+public static IdMap<ExpType,String> ex(){
 IdMap<ExpType,String> e=new IdMap<ExpType,String>(new CollCapacity(6));
-e.addEntry(ExpType.P,MbOperationNode.CARAC_FERME+"("+Cst.NIV+",1,1)+"+MbOperationNode.CARAC_DROITE_FERME+"("+Cst.NIV+",2)*(6/5*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)-15*"+MbOperationNode.PUIS+"("+Cst.NIV+",2)+100*"+Cst.NIV+"-140)");
-e.addEntry(ExpType.L,"5/4*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)");
-e.addEntry(ExpType.E,MbOperationNode.CARAC_FERME+"("+Cst.NIV+",1,50)*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)*"+MbOperationNode.DIV_FCT+"(100-"+Cst.NIV+",50)+"+MbOperationNode.CARAC_FERME+"("+Cst.NIV+",51,68)*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)*"+MbOperationNode.DIV_FCT+"((150-"+Cst.NIV+"),100)+"+MbOperationNode.CARAC_FERME+"("+Cst.NIV+",69,98)*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)*"+MbOperationNode.DIV_FCT+"("+MbOperationNode.ENT+"("+MbOperationNode.DIV_FCT+"((1911-10*"+Cst.NIV+"),3)),500)+"+MbOperationNode.CARAC_FERME+"("+Cst.NIV+",99,110)*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)*"+MbOperationNode.DIV_FCT+"((160-"+Cst.NIV+"),100)+(363*"+MbOperationNode.PUIS+"("+Cst.NIV+",2)-75027*"+Cst.NIV+"+4526170)*"+MbOperationNode.CARAC_DROITE_FERME+"("+Cst.NIV+",111)");
-e.addEntry(ExpType.F,MbOperationNode.CARAC_FERME+"("+Cst.NIV+",1,15)*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)*"+MbOperationNode.DIV_FCT+"(24+"+MbOperationNode.ENT+"("+MbOperationNode.DIV_FCT+"("+Cst.NIV+"+1,3)),50)+"+MbOperationNode.CARAC_FERME+"("+Cst.NIV+",16,35)*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)*"+MbOperationNode.DIV_FCT+"(14+"+Cst.NIV+",50)+"+MbOperationNode.CARAC_DROITE_FERME+"("+Cst.NIV+",36)*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)*"+MbOperationNode.DIV_FCT+"(32+"+MbOperationNode.ENT+"("+MbOperationNode.DIV_FCT+"("+Cst.NIV+",2)),50)");
-e.addEntry(ExpType.M,MbOperationNode.PUIS+"("+Cst.NIV+",3)");
-e.addEntry(ExpType.R,"4/5*"+MbOperationNode.PUIS+"("+Cst.NIV+",3)");
+e.addEntry(ExpType.P,MbOperationNode.CARAC_FERME+LP+Cst.NIV+OC+R_1+OC+R_1+RP+OP+MbOperationNode.CARAC_DROITE_FERME+LP+Cst.NIV+OC+R_2+RP+OM+LP+R_6_5+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP+OD+R_15+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_2+RP+OP+R_100+OM+Cst.NIV+OD+R_140+RP);
+e.addEntry(ExpType.L,R_5_4+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP);
+e.addEntry(ExpType.E,MbOperationNode.CARAC_FERME+LP+Cst.NIV+OC+R_1+OC+R_50+RP+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP+OM+MbOperationNode.DIV_FCT+LP+R_100+OD+Cst.NIV+OC+R_50+RP+OP+MbOperationNode.CARAC_FERME+LP+Cst.NIV+OC+R_51+OC+R_68+RP+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP+OM+MbOperationNode.DIV_FCT+LP+LP+R_150+OD+Cst.NIV+RP+OC+R_100+RP+OP+MbOperationNode.CARAC_FERME+LP+Cst.NIV+OC+R_69+OC+R_98+RP+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP+OM+MbOperationNode.DIV_FCT+LP+MbOperationNode.ENT+LP+MbOperationNode.DIV_FCT+LP+LP+R_1911+OD+R_10+OM+Cst.NIV+RP+OC+R_3+RP+RP+OC+R_500+RP+OP+MbOperationNode.CARAC_FERME+LP+Cst.NIV+OC+R_99+OC+R_110+RP+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP+OM+MbOperationNode.DIV_FCT+LP+LP+R_160+OD+Cst.NIV+RP+OC+R_100+RP+OP+LP+R_363+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_2+RP+OD+R_75027+OM+Cst.NIV+OP+R_4526170+RP+OM+MbOperationNode.CARAC_DROITE_FERME+LP+Cst.NIV+OC+R_111+RP);
+e.addEntry(ExpType.F,MbOperationNode.CARAC_FERME+LP+Cst.NIV+OC+R_1+OC+R_15+RP+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP+OM+MbOperationNode.DIV_FCT+LP+R_24+OP+MbOperationNode.ENT+LP+MbOperationNode.DIV_FCT+LP+Cst.NIV+OP+R_1+OC+R_3+RP+RP+OC+R_50+RP+OP+MbOperationNode.CARAC_FERME+LP+Cst.NIV+OC+R_16+OC+R_35+RP+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP+OM+MbOperationNode.DIV_FCT+LP+R_14+OP+Cst.NIV+OC+R_50+RP+OP+MbOperationNode.CARAC_DROITE_FERME+LP+Cst.NIV+OC+R_36+RP+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP+OM+MbOperationNode.DIV_FCT+LP+R_32+OP+MbOperationNode.ENT+LP+MbOperationNode.DIV_FCT+LP+Cst.NIV+OC+R_2+RP+RP+OC+R_50+RP);
+e.addEntry(ExpType.M,MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP);
+e.addEntry(ExpType.R,R_4_5+OM+MbOperationNode.PUIS+LP+Cst.NIV+OC+R_3+RP);
 return e;
 }
 }
--- desktop/applis/apps/core/scripts/scripts_game/src/main/java/aiki/sml/trs/Cst5.java
+++ desktop/applis/apps/core/scripts/scripts_game/src/main/java/aiki/sml/trs/Cst5.java
@@ -1,9 +1,10 @@
 package aiki.sml.trs;
 import aiki.db.*;
+import aiki.sml.init.*;
 import code.maths.litteral.*;
 import code.util.*;
 import aiki.game.params.enums.*;
-public final class Cst5{
+public final class Cst5 extends CstIgame {
 static final String CS_TRES_DIFFICILE=DataBase.DEF_TRES_DIFFICILE;
 static final String CS_DIFFICILE=DataBase.DEF_DIFFICILE;
 static final String CS_TRES_FACILE=DataBase.DEF_TRES_FACILE;
@@ -13,10 +14,10 @@ static final String VAR_LEVEL_WINNER= MessagesDataBaseConstants.VAR_DEF+DataBase
 private Cst5(){}
 static IdMap<DifficultyWinPointsFight,String> di(){
 IdMap<DifficultyWinPointsFight,String> e=new IdMap<DifficultyWinPointsFight,String>(new CollCapacity(4));
-e.addEntry(DifficultyWinPointsFight.TRES_FACILE,MbOperationNode.CARAC_FERME+"("+MbOperationNode.DIV_FCT+"("+VAR_LEVEL_LOOSER+","+VAR_LEVEL_WINNER+"),0,1)+"+MbOperationNode.CARAC_DROITE_OUVERT+"("+MbOperationNode.DIV_FCT+"("+VAR_LEVEL_LOOSER+","+VAR_LEVEL_WINNER+"),1)*"+MbOperationNode.PUIS+"("+MbOperationNode.DIV_FCT+"("+VAR_LEVEL_LOOSER+","+VAR_LEVEL_WINNER+"),3)");
-e.addEntry(DifficultyWinPointsFight.FACILE,MbOperationNode.PUIS+"("+MbOperationNode.DIV_FCT+"("+VAR_LEVEL_LOOSER+","+VAR_LEVEL_WINNER+"),3)");
-e.addEntry(DifficultyWinPointsFight.DIFFICILE,"1");
-e.addEntry(DifficultyWinPointsFight.TRES_DIFFICILE,MbOperationNode.CARAC_FERME+"("+MbOperationNode.DIV_FCT+"("+VAR_LEVEL_LOOSER+","+VAR_LEVEL_WINNER+"),0,1)*"+MbOperationNode.PUIS+"("+MbOperationNode.DIV_FCT+"("+VAR_LEVEL_LOOSER+","+VAR_LEVEL_WINNER+"),3)+"+MbOperationNode.CARAC_DROITE_OUVERT+"("+MbOperationNode.DIV_FCT+"("+VAR_LEVEL_LOOSER+","+VAR_LEVEL_WINNER+"),1)");
+e.addEntry(DifficultyWinPointsFight.TRES_FACILE,MbOperationNode.CARAC_FERME+LP+MbOperationNode.DIV_FCT+LP+VAR_LEVEL_LOOSER+OC+VAR_LEVEL_WINNER+RP+OC+R_0+OC+R_1+RP+OP+MbOperationNode.CARAC_DROITE_OUVERT+LP+MbOperationNode.DIV_FCT+LP+VAR_LEVEL_LOOSER+OC+VAR_LEVEL_WINNER+RP+OC+R_1+RP+OM+MbOperationNode.PUIS+LP+MbOperationNode.DIV_FCT+LP+VAR_LEVEL_LOOSER+OC+VAR_LEVEL_WINNER+RP+OC+R_3+RP);
+e.addEntry(DifficultyWinPointsFight.FACILE,MbOperationNode.PUIS+LP+MbOperationNode.DIV_FCT+LP+VAR_LEVEL_LOOSER+OC+VAR_LEVEL_WINNER+RP+OC+R_3+RP);
+e.addEntry(DifficultyWinPointsFight.DIFFICILE,R_1);
+e.addEntry(DifficultyWinPointsFight.TRES_DIFFICILE,MbOperationNode.CARAC_FERME+LP+MbOperationNode.DIV_FCT+LP+VAR_LEVEL_LOOSER+OC+VAR_LEVEL_WINNER+RP+OC+R_0+OC+R_1+RP+OM+MbOperationNode.PUIS+LP+MbOperationNode.DIV_FCT+LP+VAR_LEVEL_LOOSER+OC+VAR_LEVEL_WINNER+RP+OC+R_3+RP+OP+MbOperationNode.CARAC_DROITE_OUVERT+LP+MbOperationNode.DIV_FCT+LP+VAR_LEVEL_LOOSER+OC+VAR_LEVEL_WINNER+RP+OC+R_1+RP);
 return e;
 }
 }
