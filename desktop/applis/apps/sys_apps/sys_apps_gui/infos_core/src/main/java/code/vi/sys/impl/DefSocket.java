a63c7445d0228ee87b629e4c7d2699ee85d37332
Cardman <f.desrochettes@gmail.com>
2024-11-28 19:59:15 +0100

    try improving read / write about files and network

--- desktop/applis/apps/sys_apps/sys_apps_gui/infos_core/src/main/java/code/vi/sys/impl/DefSocket.java
+++ desktop/applis/apps/sys_apps/sys_apps_gui/infos_core/src/main/java/code/vi/sys/impl/DefSocket.java
@@ -1,8 +1,8 @@
 package code.vi.sys.impl;
 
 import code.gui.initialize.*;
-import code.stream.*;
-import code.util.core.*;
+//import code.stream.*;
+//import code.util.core.*;
 import code.vi.prot.impl.*;
 
 import java.io.*;
@@ -23,31 +23,53 @@ public final class DefSocket implements AbstractSocket {
     @Override
     public String read() {
         try {
-            return decode(StreamBinaryFile.readBytes(new DefBufferedReader(socket.getInputStream())));
+            BufferedReader br_ = new BufferedReader(new InputStreamReader(socket.getInputStream(),StreamCoreUtil.utf()));
+            return br_.readLine();
         } catch (Exception e) {
             return null;
         }
     }
 
-    private static String decode(BytesInfo _info) {
-        try {
-            return StringUtil.decode(_info.getBytes());
-        } catch (Exception e) {
-            return null;
-        }
-    }
+//    @Override
+//    public BytesInfo readBytes() {
+//        try {
+//            return StreamBinaryFile.readBytes(new DefBufferedReader(socket.getInputStream()));
+//        } catch (Exception e) {
+//            return null;
+//        }
+//    }
+
+//    private static String decode(BytesInfo _info) {
+//        try {
+//            return StringUtil.decode(_info.getBytes());
+//        } catch (Exception e) {
+//            return null;
+//        }
+//    }
 
     @Override
     public String println(String _string) {
         try {
-            OutputStream out_ = socket.getOutputStream();
-            out_.write(StringUtil.encode(_string+"\n"));
+            BufferedWriter out_ = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),StreamCoreUtil.utf()));
+            out_.write(_string+"\n");
             out_.flush();
             return _string+"\n";
         } catch (Exception e) {
             return "";
         }
     }
+
+//    @Override
+//    public byte[] println(byte[] _string) {
+//        try {
+//            OutputStream out_ = socket.getOutputStream();
+//            out_.write(_string);
+//            out_.flush();
+//            return _string;
+//        } catch (Exception e) {
+//            return new byte[0];
+//        }
+//    }
 
     @Override
     public void close() {
